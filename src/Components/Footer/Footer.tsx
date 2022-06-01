1892a6dfb5905606af9ba9d64147a1149d30806c
tohid <tohidebrahimi17@gmail.com>
2022-06-01 17:52:19 +0430

    Footerlist created and imported

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import FooterList from "./FooterList/FooterList";
+import styles from "./Footer.module.css";
+import { Grid } from "@mui/material";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <Grid
+        container
+        sx={{ display: "flex", bgcolor: "#092240" }}
+        height="100%"
+        justifyContent="space-around"
+        alignItems="center"
+      >
+        <Grid item xs={12} sm={6} md={4} lg={2}>
+          <FooterList />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={2}>
+          <FooterList />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={2}>
+          <FooterList />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={2}>
+          <FooterList />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Footer;
