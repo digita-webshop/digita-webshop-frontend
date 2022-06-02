6c4863f51cac32afe41cdef92b72710b2505cf75
Siavash <siavash.khani200@gmail.com>
2022-06-02 15:41:54 +0430

    updated Products.tsx

---
+++ src/Components/products/Products.tsx
@@ -0,0 +1,37 @@
+import { Box, Container, Grid } from "@mui/material";
+import React from "react";
+import ProductHeader from "./components/header/ProductHeader";
+import ProductItem from "./components/productItem/ProductItem";
+import BasicBtn from "./components/button/BasicBtn";
+import { productData } from "../../Services/Utils/data";
+import OtherProducts from "./components/otherProducts/OtherProducts";
+
+const Products = () => {
+  return (
+    <Container>
+      <ProductHeader />
+
+      <Box>
+        <Grid container spacing={{ xs: 2, md: 3 }}>
+          {productData.map((item) => (
+              <Grid item xs={6} sm={4} md={3}>
+                <ProductItem
+                  id={item.id}
+                  name={item.name}
+                  image={item.image}
+                  offPrice={item.offPrice}
+                  price={item.price}
+                  sold={item.sold}
+                  starRate={item.starRate}
+                />
+              </Grid>
+              ))}
+          </Grid>
+      </Box>
+      <BasicBtn />
+      <OtherProducts />
+    </Container>
+  );
+};
+
+export default Products;
