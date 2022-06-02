70a5168949e5770a49ed52c831c55a98d3e7dce3
Siavash <siavash.khani200@gmail.com>
2022-06-02 15:30:35 +0430

    data file added to Services/Utils

---
+++ src/Services/Utils/data.js
@@ -0,0 +1,10 @@
+export const productData = [
+    { id: 1, name: 'Beats by Dr. Dre Studio...', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2018/09/product-14-330x330.jpg', offPrice: 79.00, price: 69.00, sold: true, starRate: 5 },
+    { id: 2, name: 'Corsair HS35 Headphones(Red)', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2020/11/product-1-330x330.jpg', offPrice: 0, price: 70.00, sold: false, starRate: 5 },
+    { id: 3, name: 'Microsoft Xbox One S Blue Grey', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2020/11/product-7-330x330.jpg', offPrice: 0, price: 52.00, sold: false, starRate: 5 },
+    { id: 4, name: 'Sam Sung Galaxy Note 10 Lite', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2020/11/product-4-330x330.jpg', offPrice: 79.00, price: 52.00, sold: true, starRate: 5 },
+    { id: 5, name: 'Samsung Galaxy M21', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2020/11/product-2-330x330.jpg', offPrice: 79.00, price: 55.00, sold: true, starRate: 0 },
+    { id: 6, name: 'Samsung Galaxy V21', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2018/09/product-16-330x330.jpg', offPrice: 0, price: 75.00, sold: false, starRate: 5 },
+    { id: 7, name: 'SIRUM 360 Mobile Holder Trip', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2020/11/product-8-330x330.jpg', offPrice: 0, price: 12.00, sold: false, starRate: 5 },
+    { id: 8, name: 'SmartPhone & Ipad', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2018/09/product-14-330x330.jpg', offPrice: 0, price: 75.00, sold: false, starRate: 5 },
+]
