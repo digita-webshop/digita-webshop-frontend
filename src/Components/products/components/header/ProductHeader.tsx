05d66718af6e5886d6b52f9a23e802f7e7e7dfec
Siavash <siavash.khani200@gmail.com>
2022-06-02 15:35:23 +0430

    header added to product components

---
+++ src/Components/products/components/header/ProductHeader.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box, Button, styled, Typography } from "@mui/material";
+
+const StyledProducts = styled(Box)(({ theme }) => ({
+  paddingTop: "3rem",
+  display: "flex",
+  justifyContent: "space-between",
+  paddingBottom: "1.5rem",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
+}));
+
+
+
+const ProductHeader = () => {
+  return (
+    <Box>
+      <StyledProducts>
+        <Typography variant="h5" component="h2" fontWeight={400}>
+          TOP PRODUCTS
+        </Typography>
+        <Box>
+          <Button variant="text" sx={{ color: "gray", fontSize: '0.9rem' }}>
+            AUDIO & VIDEO GAME
+          </Button>
+          <Button variant="text" sx={{ color: "gray",fontSize: '0.9rem' }}>
+            CAMERA & PHOTO
+          </Button>
+          <Button variant="text" sx={{ color: "gray",fontSize: '0.9rem' }}>
+            HEADPHONE
+          </Button>
+        </Box>
+      </StyledProducts>
+    </Box>
+  );
+};
+
+export default ProductHeader;
