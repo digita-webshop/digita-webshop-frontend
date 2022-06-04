05deb8e57767f2b281bfee43c0321209d4b7984a
tohid <tohidebrahimi17@gmail.com>
2022-06-04 21:36:29 +0430

    style(navbar):add shopping card

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -13,10 +13,16 @@ import {
   ListItemText,
   Divider,
   InputBase,
+  ImageListItem,
+  Menu,
+  MenuItem,
+  Button,
+  ListItemIcon,
+  useMediaQuery,
 } from "@mui/material";
 import logoImg from "../../Assets/Images/digita-logo.png";
 import { navbarItems } from "../../Assets/Data/Data";
-import { styled } from "@mui/material/styles";
+import { styled, useTheme } from "@mui/material/styles";
 import {
   LocalGroceryStoreOutlined,
   SearchOutlined,
@@ -71,9 +77,27 @@ const navbarIcons = {
 function Navbar() {
   const [selectedTab, setSelectedTab] = useState(0);
   const [displayDrawer, setDisplayDrawer] = useState(false);
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("md"));
+  console.log(matches);
+  const handleClick = () => {
+    if (matches) {
+      setDisplayMenu(true);
+    }
+  };
+  const handleClose = () => {
+    if (matches) {
+      setDisplayMenu(false);
+    }
+  };
   return (
     <AppBar sx={{ backgroundColor: "white" }}>
-      <Container maxWidth={"xl"}>
+      <Container
+        maxWidth={"xl"}
+        sx={{ position: "relative" }}
+        onMouseEnter={handleClose}
+      >
         <Toolbar
           sx={{
             justifyContent: "space-between",
@@ -153,7 +177,7 @@ function Navbar() {
               </Box>
               <List>
                 {navbarItems.map((item) => (
-                  <Fragment>
+                  <Fragment key={item.id}>
                     <ListItem>
                       <ListItemText
                         primary={item.name}
@@ -179,10 +203,17 @@ function Navbar() {
             onChange={(event, newValue) => setSelectedTab(newValue)}
           >
             {navbarItems.map((item) => (
-              <AntTab label={item.name} sx={{ height: "90px" }} />
+              <AntTab key={item.id} label={item.name} sx={{ height: "90px" }} />
             ))}
           </AntTabs>
-          <Box marginLeft={"auto"} sx={{ marginLeft: { sx: "0", md: "auto" } }}>
+          <Box
+            sx={{
+              marginLeft: { sx: "0", md: "auto" },
+              height: { md: "90px" },
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
             <Badge
               showZero
               sx={{
@@ -205,33 +236,137 @@ function Navbar() {
             >
               <LoginOutlined color="primary" sx={navbarIcons} />
             </Badge>
-            <Badge
-              badgeContent={4}
-              overlap="circular"
-              color="error"
-              sx={{
-                display: {
-                  xs: "none",
-                  md: "inline-flex",
-                },
-              }}
-            >
-              <FavoriteBorderOutlined color="primary" sx={navbarIcons} />
-            </Badge>
-            <Badge badgeContent={4} overlap="circular" color="error">
-              <LocalGroceryStoreOutlined
-                color="primary"
+            <Box display={"flex"} height={"100%"} onMouseEnter={handleClose}>
+              <Badge
+                badgeContent={4}
+                overlap="circular"
+                color="error"
                 sx={{
-                  marginLeft: "12px",
-                  transition: "all 200ms",
-                  cursor: "pointer",
-                  fontSize: "28px",
-                  "&:hover": { color: "#f03637" },
+                  display: {
+                    xs: "none",
+                    md: "inline-flex",
+                  },
+                  margin: "auto",
                 }}
-              />
-            </Badge>
+              >
+                <FavoriteBorderOutlined color="primary" sx={navbarIcons} />
+              </Badge>
+            </Box>
+            <Box onMouseEnter={handleClick}>
+              <Badge
+                badgeContent={4}
+                overlap="circular"
+                color="error"
+                sx={{ margin: "auto  " }}
+              >
+                <LocalGroceryStoreOutlined
+                  color="primary"
+                  sx={{
+                    marginLeft: "12px",
+                    transition: "all 200ms",
+                    cursor: "pointer",
+                    fontSize: "28px",
+                    "&:hover": { color: "#f03637" },
+                  }}
+                />
+              </Badge>
+            </Box>
           </Box>
         </Toolbar>
+        <Box
+          display={`${displayMenu ? "inline-block" : "none"}`}
+          sx={{
+            position: "absolute",
+            right: "10px",
+            top: "72px ",
+            padding: "15px",
+          }}
+          onMouseLeave={handleClose}
+        >
+          <List sx={{ maxHeight: "360px", overflow: "auto" }}>
+            <ListItem disableGutters>
+              <img
+                src="https://demo-61.woovinapro.com/wp-content/uploads/2018/09/product-16-330x330.jpg"
+                alt="product"
+                width={75}
+                height={75}
+              />
+              <ListItemText sx={{ marginLeft: "16px", marginRight: "40px" }}>
+                <Typography variant="body2" color={"primary"}>
+                  Samsung Galaxy V21
+                </Typography>
+                <Box sx={{ display: "flex", alignItems: "center", gap: "4px" }}>
+                  <Typography variant="subtitle1" color={"secondary"}>
+                    1 x{" "}
+                  </Typography>
+                  <Typography variant="body2" color={"primary"}>
+                    $75.00{" "}
+                  </Typography>
+                </Box>
+              </ListItemText>
+              <ListItemIcon>
+                <Box>
+                  <CloseRounded
+                    sx={{
+                      border: "1px solid black",
+                      borderRadius: "50%",
+                      display: "flex",
+                      justifyContent: "center",
+                      padding: "3px",
+                      "&:hover": {
+                        border: "1px solid red",
+                        color: "red",
+                      },
+                      cursor: "pointer",
+                    }}
+                  />
+                </Box>
+              </ListItemIcon>
+            </ListItem>
+            <Divider />
+          </List>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              padding: "13px",
+            }}
+          >
+            <Typography
+              variant="subtitle2"
+              color={"secondary"}
+              fontWeight={400}
+            >
+              SUBTOTAL:
+            </Typography>
+            <Typography variant="subtitle2" color={"primary"}>
+              $325:00
+            </Typography>
+          </Box>
+          <Divider />
+          <Box
+            sx={{
+              display: "flex",
+              paddingY: "20px",
+              justifyContent: "space-between",
+              gap: "10px",
+            }}
+          >
+            <Button
+              variant="contained"
+              fullWidth={true}
+              color="error"
+              sx={{
+                "&:hover": { backgroundColor: "#333333" },
+              }}
+            >
+              VIEW CARD
+            </Button>
+            <Button variant="contained" fullWidth={true}>
+              CHECKOUT
+            </Button>
+          </Box>
+        </Box>
       </Container>
     </AppBar>
   );
