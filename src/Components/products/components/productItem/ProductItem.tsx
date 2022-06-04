44535cb8b13505bed1f570dc54d97d945b2ad03c
Siavash <siavash.khani200@gmail.com>
2022-06-04 23:18:26 +0430

    feat(ProductItem): added wishlist & view product modals

--- src/Components/products/components/productItem/ProductItem.tsx
+++ src/Components/products/components/productItem/ProductItem.tsx
@@ -1,13 +1,17 @@
+import React, { useState } from "react";
 import {
   Box,
+  Button,
   Card,
   CardActionArea,
   CardContent,
   CardMedia,
+  Grid,
   Rating,
   styled,
   Typography,
 } from "@mui/material";
+import Modal from "@mui/material/Modal";
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
 import StarIcon from "@mui/icons-material/Star";
@@ -15,6 +19,8 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
 import VisibilityIcon from "@mui/icons-material/Visibility";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
 
 type Props = {
   name: string;
@@ -37,15 +43,38 @@ const Div = styled("div")(({ theme }) => ({
 }));
 
 const StyledIcons = styled("div")(({ theme }) => ({
-  position: 'absolute',
-  top: '85%',
-  left: '50%',
-  transform: 'translate(-50%,-50%)',
+  position: "absolute",
+  top: "85%",
+  left: "50%",
+  transform: "translate(-50%,-50%)",
 
   backgroundColor: "#E9E9E9",
   width: "150px",
   borderRadius: "7px",
 }));
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: 3,
+  width: 300,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  Button: {
+    width : '100%',
+    display: 'flex',
+    padding: '1rem 0',
+    textTransform: 'uppercase'
+  }
+};
 
 const ProductItem = ({
   name,
@@ -56,81 +85,137 @@ const ProductItem = ({
   sold,
   starRate,
 }: Props) => {
+  const [openView, setOpenView] = useState(false);
+  const [openWish, setOpenWish] = useState(false);
+
   return (
-    <Card
-      sx={{ maxWidth: 270, boxShadow: "5px 4px 10px 1px rgba(0,0,0,0.12)" }}
-    >
-      <CardActionArea>
-        <Box sx={{position: 'relative'}}>
-          <Div sx={{ fontSize: "12px" }}>{sold && "Sale!"}</Div>
-          <CardMedia
-            component="img"
-            height="260"
-            image={image}
-            alt="green iguana"
-          />
+    <Grid item xs={6} sm={4} md={3} key={id}>
+      <Card
+        sx={{ maxWidth: 270, boxShadow: "5px 4px 10px 1px rgba(0,0,0,0.12)" }}
+      >
+        <CardActionArea>
+          <Box sx={{ position: "relative" }}>
+            <Div sx={{ fontSize: "12px" }}>{sold && "Sale!"}</Div>
+            <CardMedia
+              component="img"
+              height="260"
+              image={image}
+              alt="green iguana"
+            />
 
-          {/* Product Item Icons */}
-          <StyledIcons>
-            <Stack direction="row">
-              <IconButton sx={{ color: "gray" }} aria-label="delete">
-                <ShoppingCartIcon fontSize="small" />
-              </IconButton>
-              <IconButton sx={{ color: "gray" }} aria-label="delete" disabled>
-                <FavoriteBorderIcon fontSize="small" />
-              </IconButton>
-              <IconButton sx={{ color: "gray" }} aria-label="add an alarm">
-                <CompareArrowsIcon fontSize="small" />
-              </IconButton>
-              <IconButton
-                sx={{ color: "gray" }}
-                aria-label="add to shopping cart"
-              >
-                <VisibilityIcon fontSize="small" />
-              </IconButton>
-            </Stack>
-          </StyledIcons>
-          {/* Product Item Icons */}
-        </Box>
+            {/* Product Item Icons */}
+            <StyledIcons>
+              <Stack direction="row">
+                <IconButton sx={{ color: "gray" }} aria-label="delete">
+                  <ShoppingCartIcon fontSize="small" />
+                </IconButton>
+                <IconButton sx={{ color: "gray" }} aria-label="delete" onClick={() => setOpenWish(true)}>
+                  <FavoriteBorderIcon fontSize="small" />
+                </IconButton>
+                <IconButton sx={{ color: "gray" }} aria-label="add an alarm">
+                  <CompareArrowsIcon fontSize="small" />
+                </IconButton>
+                <IconButton
+                  sx={{ color: "gray" }}
+                  aria-label="add to shopping cart"
+                  onClick={() => setOpenView(true)}
+                >
+                  <VisibilityIcon fontSize="small" />
+                </IconButton>
+              </Stack>
+            </StyledIcons>
+            {/* Product Item Icons */}
+          </Box>
 
-        <CardContent>
-          <Rating
-            name="text-feedback"
-            size="small"
-            value={starRate}
-            readOnly
-            precision={0.5}
-            emptyIcon={
-              <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
-            }
-          />
-          <Typography
-            gutterBottom
-            variant="body2"
-            component="div"
-            sx={{ margin: "0.2rem" }}
-            fontWeight={500}
+          {/* =========== Wishlist ======== */}
+
+          <Modal
+            open={openWish}
+            onClose={() => setOpenWish(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
           >
-            {name}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{ margin: "0.2rem" }}
+            <Box sx={style}>
+              
+              <FavoriteIcon sx={{ fontSize: 50, color: '#f03637' }} />
+              <Typography id="modal-modal-title" variant="h5" component="h2">
+                Product added to Wishlist
+              </Typography>
+              <Button variant="contained">
+                <FavoriteBorderIcon sx={{marginRight: '0.3rem'}}/>
+                View Wishlist</Button>
+            </Box>
+          </Modal>
+          {/* =========== Wishlist ======== */}
+
+          {/* ============ MODAL ============*/}
+          <Modal
+            open={openView}
+            onClose={() => setOpenView(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
           >
-            <Box
-              component="span"
-              sx={{ marginRight: "0.5rem", textDecoration: "line-through" }}
+            <Box sx={style}>
+              <img src={image} alt='productImage'/>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                {name}
+              </Typography>
+              <Rating
+              name="text-feedback"
+              size="small"
+              value={starRate}
+              readOnly
+              precision={0.5}
+              emptyIcon={
+                <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+              }
+            />
+              <Typography variant="h3">${price}</Typography>
+              <Button variant="contained">Add to Cart</Button>
+            </Box>
+          </Modal>
+
+          {/* ============ MODAL ============*/}
+
+          <CardContent>
+            <Rating
+              name="text-feedback"
+              size="small"
+              value={starRate}
+              readOnly
+              precision={0.5}
+              emptyIcon={
+                <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+              }
+            />
+            <Typography
+              gutterBottom
+              variant="body2"
+              component="div"
+              sx={{ margin: "0.2rem" }}
+              fontWeight={500}
             >
-              {offPrice !== 0 && `$${offPrice}.00`}
-            </Box>
-            <Box component="span" sx={{ color: "red", fontWeight: "bold" }}>
-              {`$${price}.00`}
-            </Box>
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+              {name}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ margin: "0.2rem" }}
+            >
+              <Box
+                component="span"
+                sx={{ marginRight: "0.5rem", textDecoration: "line-through" }}
+              >
+                {offPrice !== 0 && `$${offPrice}.00`}
+              </Box>
+              <Box component="span" sx={{ color: "red", fontWeight: "bold" }}>
+                {`$${price}.00`}
+              </Box>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
   );
 };
 
