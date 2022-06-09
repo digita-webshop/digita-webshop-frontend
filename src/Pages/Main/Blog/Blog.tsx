ffae25315c0ed6d98168d2d550ee17bc85785e34
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-06-09 16:05:25 +0430

    feat(header):add blog page header

--- src/Pages/Main/Blog/Blog.tsx
+++ src/Pages/Main/Blog/Blog.tsx
@@ -1,7 +1,35 @@
+import { Grid, Typography } from '@mui/material';
+import bgImage from '../../../Assets/Images/bg-page-title.jpg';
+import CustomSeparator from '../../../Components/CustomSeparator/CustomSeparator';
+
 function Blog() {
   return (
     <>
-      <div>blog page</div>
+      <Grid
+        container
+        sx={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+        height='200px'
+        display='flex'
+        flexDirection={'column'}
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Typography
+          align='center'
+          variant='h5'
+          color='white'
+          style={{ fontWeight: 600 }}
+          p={1}
+        >
+          Blog
+        </Typography>
+        <CustomSeparator name='Blog'></CustomSeparator>
+      </Grid>
     </>
   );
 }
