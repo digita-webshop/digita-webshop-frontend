fb32fbfdecf05679abcd78a87f03013a65c0e94d
tohid <tohidebrahimi17@gmail.com>
2022-06-11 20:44:22 +0430

    feat(shoppage):finished sidebar categories filter

--- src/Pages/Main/Shop/Shop.tsx
+++ src/Pages/Main/Shop/Shop.tsx
@@ -1,32 +1,12 @@
-import {
-  Grid,
-  List,
-  Box,
-  Typography,
-  ListItem,
-  Container,
-} from "@mui/material";
-import { FilterTitleWrapper } from "../../../Styles/ShopPage/index";
+import { Grid, Container } from "@mui/material";
+import { ShopCategoriesFilter } from "../../../Components";
+
 function Shop() {
   return (
     <Container sx={{ marginY: "50px" }}>
       <Grid container columnSpacing={4}>
         <Grid item xs={3.5}>
-          <Box sx={{ padding: "20px", border: "1px solid #e9e9e9" }}>
-            <FilterTitleWrapper>
-              <Typography
-                component={"h4"}
-                variant="body1"
-                color="primary"
-                fontWeight={600}
-              >
-                CATEGORIES
-              </Typography>
-            </FilterTitleWrapper>
-            <List>
-              <ListItem></ListItem>
-            </List>
-          </Box>
+          <ShopCategoriesFilter />
         </Grid>
         <Grid item xs={8.5}>
           ddd
