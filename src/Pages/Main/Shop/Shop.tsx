47e4085e0c32e430cff755562570453866f7935f
tohid <tohidebrahimi17@gmail.com>
2022-06-11 19:42:10 +0430

    feat(shoppPage):add filter section custom title

--- src/Pages/Main/Shop/Shop.tsx
+++ src/Pages/Main/Shop/Shop.tsx
@@ -1,8 +1,38 @@
+import {
+  Grid,
+  List,
+  Box,
+  Typography,
+  ListItem,
+  Container,
+} from "@mui/material";
+import { FilterTitleWrapper } from "../../../Styles/ShopPage/index";
 function Shop() {
   return (
-    <>
-      <div>shop page</div>
-    </>
+    <Container sx={{ marginY: "50px" }}>
+      <Grid container columnSpacing={4}>
+        <Grid item xs={3.5}>
+          <Box sx={{ padding: "20px", border: "1px solid #e9e9e9" }}>
+            <FilterTitleWrapper>
+              <Typography
+                component={"h4"}
+                variant="body1"
+                color="primary"
+                fontWeight={600}
+              >
+                CATEGORIES
+              </Typography>
+            </FilterTitleWrapper>
+            <List>
+              <ListItem></ListItem>
+            </List>
+          </Box>
+        </Grid>
+        <Grid item xs={8.5}>
+          ddd
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
