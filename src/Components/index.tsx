fcdb22d4f8fb19fcfe0bbcc7a7fa26389f4345c4
tohid <tohidebrahimi17@gmail.com>
2022-06-12 16:35:07 +0430

    feat(shoppage):add color filter to sidebar

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Footer } from "./Footer/Footer";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as ShopCategoriesFilter } from "./ShopCategoriesFilter/ShopCategoriesFilter";
+export { default as ShopColorFilter } from "./ShopColorFilter/ShopColorFilter";
