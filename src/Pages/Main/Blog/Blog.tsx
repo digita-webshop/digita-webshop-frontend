128a68cb084d88244dee1d06a66489a08ac0daad
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-06-16 22:35:57 +0430

    feat(article):add container & image first article

--- src/Pages/Main/Blog/Blog.tsx
+++ src/Pages/Main/Blog/Blog.tsx
@@ -1,13 +1,17 @@
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 import { Grid, Typography } from '@mui/material';
-import bgImage from '../../../Assets/Images/bg-page-title.jpg';
+import IMAGES from '../../../Assets/Images';
 import CustomSeparator from '../../../Components/CustomSeparator/CustomSeparator';
+import Link from '@mui/material/Link';
+
 
 const bgStyles = {
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-  backgroundImage: `url(${bgImage})`,
+  backgroundImage: `url(${IMAGES.bgImage})`,
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
@@ -27,12 +31,25 @@ function Blog() {
         <Typography
           align='center'
           color='white'
-          sx={{fontWeight: 600, fontSize: {xs: '1.2rem', sm: '2.2rem'}}}
+          sx={{ fontWeight: 600, fontSize: { xs: '1.2rem', sm: '2.2rem' } }}
           p={1}
         >
           Blog
         </Typography>
         <CustomSeparator name='Blog'></CustomSeparator>
+      </Grid>
+      <Grid container sx={{ py: 6.25, px: 2 }}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card>
+            <Link href='#'>
+              <CardMedia
+                component='img'
+                image={`${IMAGES.blog6}`}
+                alt='On Salvatore Ferragamo’s New Chapter'
+              />
+            </Link>
+          </Card>
+        </Grid>
       </Grid>
     </>
   );
