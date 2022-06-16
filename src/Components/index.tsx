dfedd6976a319ea61fcaf31a9c1f2fe586def13c
tohid <tohidebrahimi17@gmail.com>
2022-06-16 16:55:49 +0430

    feat(shoppage):added breadccrumbs cp

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -6,3 +6,4 @@ export { default as ShopToolbar } from "./ShopToolbar/ShopToolbar";
 export { default as Special } from "./Special/Special";
 export { default as Products } from "./Products/Products";
 export { default as Header } from "./Header/Header";
+export { default as BreadcrumbsCp } from "./BreadcrumbsCp/BreadcrumbsCp";
