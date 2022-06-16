a4cff1c424cb46da20bae797d3bdfc2132d942f9
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-06-16 23:45:30 +0430

    feat(articles):complete all articles with responsive

--- src/Pages/Main/Blog/Blog.tsx
+++ src/Pages/Main/Blog/Blog.tsx
@@ -53,78 +53,484 @@ function Blog() {
         <CustomSeparator name='Blog'></CustomSeparator>
       </Grid>
       <Grid container sx={{ py: 6.25, px: 2 }}>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card>
-            <Link href='#'>
-              <CardMedia
-                component='img'
-                image={`${IMAGES.blog6}`}
-                alt='On Salvatore Ferragamo’s New Chapter'
-              />
-            </Link>
-            <Box display='flex' flexDirection='row' alignItems='flex-end'>
-              <Item>
-                <Box display='flex' alignItems='flex-end'>
-                  <PersonOutlineOutlinedIcon
-                    style={{ fontSize: '1rem' }}
-                    color='secondary'
+        <Grid item md={9}>
+          <Grid container rowSpacing={5} columnSpacing={3}>
+            <Grid item xs={12} sm={6}>
+              <Card>
+                <Link href='#'>
+                  <CardMedia
+                    component='img'
+                    image={`${IMAGES.blog6}`}
+                    alt='On Salvatore Ferragamo’s New Chapter'
                   />
-                  <Link
-                    underline='none'
-                    href='#'
-                    color='secondary'
-                    sx={{
-                      '&:hover': { color: '#f03637', transition: 'all 500ms' },
-                    }}
-                  >
-                    Peter Giroud -
-                  </Link>
+                </Link>
+                <Box display='flex' flexDirection='row' alignItems='flex-end'>
+                  <Item>
+                    <Box display='flex' alignItems='flex-end'>
+                      <PersonOutlineOutlinedIcon
+                        style={{ fontSize: '1rem' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        Peter Giroud -
+                      </Link>
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <AccessTimeOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      &nbsp;July 9, 2019 -
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <FolderOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        &nbsp;Browse Our Sales
+                      </Link>
+                    </Box>
+                  </Item>
                 </Box>
-              </Item>
-              <Item>
-                <Box display='flex'>
-                  &nbsp;
-                  <AccessTimeOutlinedIcon
-                    style={{ fontSize: '13px' }}
-                    color='secondary'
+                <Link
+                  sx={{
+                    '&:hover': { color: '#f03637', transition: 'all 500ms' },
+                  }}
+                  variant='body1'
+                  fontWeight={500}
+                  underline='none'
+                  key='1'
+                  href='#'
+                >
+                  On Salvatore Ferragamo’s New Chapter
+                </Link>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Card>
+                <Link href='#'>
+                  <CardMedia
+                    component='img'
+                    image={`${IMAGES.blog5}`}
+                    alt='On Salvatore Ferragamo’s New Chapter'
                   />
-                  &nbsp;July 9, 2019 -
+                </Link>
+                <Box display='flex' flexDirection='row' alignItems='flex-end'>
+                  <Item>
+                    <Box display='flex' alignItems='flex-end'>
+                      <PersonOutlineOutlinedIcon
+                        style={{ fontSize: '1rem' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        Peter Giroud -
+                      </Link>
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <AccessTimeOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      &nbsp;October 5, 2018 -
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <FolderOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        &nbsp;Men
+                      </Link>
+                    </Box>
+                  </Item>
                 </Box>
-              </Item>
-              <Item>
-                <Box display='flex'>
-                  &nbsp;
-                  <FolderOutlinedIcon
-                    style={{ fontSize: '13px' }}
-                    color='secondary'
+                <Link
+                  sx={{
+                    '&:hover': { color: '#f03637', transition: 'all 500ms' },
+                  }}
+                  variant='body1'
+                  fontWeight={500}
+                  underline='none'
+                  key='1'
+                  href='#'
+                >
+                  Decoding #OOTD Pose in The Modeling Industry
+                </Link>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Card>
+                <Link href='#'>
+                  <CardMedia
+                    component='img'
+                    image={`${IMAGES.blog4}`}
+                    alt='On Salvatore Ferragamo’s New Chapter'
                   />
-                  <Link
-                    underline='none'
-                    href='#'
-                    color='secondary'
-                    sx={{
-                      '&:hover': { color: '#f03637', transition: 'all 500ms' },
-                    }}
-                  >
-                    &nbsp;Browse Our Sales
-                  </Link>
+                </Link>
+                <Box display='flex' flexDirection='row' alignItems='flex-end'>
+                  <Item>
+                    <Box display='flex' alignItems='flex-end'>
+                      <PersonOutlineOutlinedIcon
+                        style={{ fontSize: '1rem' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        Peter Giroud -
+                      </Link>
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <AccessTimeOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      &nbsp;June 10, 2018 -
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <FolderOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        &nbsp;On Sale
+                      </Link>
+                    </Box>
+                  </Item>
                 </Box>
-              </Item>
-            </Box>
-            <Link
-              sx={{
-                '&:hover': { color: '#f03637', transition: 'all 500ms' },
-              }}
-              variant='body2'
-              fontWeight={500}
-              underline='none'
-              key='1'
-              href='#'
-            >
-              On Salvatore Ferragamo’s New Chapter
-            </Link>
-          </Card>
+                <Link
+                  sx={{
+                    '&:hover': { color: '#f03637', transition: 'all 500ms' },
+                  }}
+                  variant='body1'
+                  fontWeight={500}
+                  underline='none'
+                  key='1'
+                  href='#'
+                >
+                  Decoding #OOTD Pose in The Modeling Industry
+                </Link>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Card>
+                <Link href='#'>
+                  <CardMedia
+                    component='img'
+                    image={`${IMAGES.blog3}`}
+                    alt='On Salvatore Ferragamo’s New Chapter'
+                  />
+                </Link>
+                <Box display='flex' flexDirection='row' alignItems='flex-end'>
+                  <Item>
+                    <Box display='flex' alignItems='flex-end'>
+                      <PersonOutlineOutlinedIcon
+                        style={{ fontSize: '1rem' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        Peter Giroud -
+                      </Link>
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <AccessTimeOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      &nbsp;June 8, 2018 -
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <FolderOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        &nbsp;Women
+                      </Link>
+                    </Box>
+                  </Item>
+                </Box>
+                <Link
+                  sx={{
+                    '&:hover': { color: '#f03637', transition: 'all 500ms' },
+                  }}
+                  variant='body1'
+                  fontWeight={500}
+                  underline='none'
+                  key='1'
+                  href='#'
+                >
+                  Sample Post With Format Chat
+                </Link>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Card>
+                <Link href='#'>
+                  <CardMedia
+                    component='img'
+                    image={`${IMAGES.blog2}`}
+                    alt='On Salvatore Ferragamo’s New Chapter'
+                  />
+                </Link>
+                <Box display='flex' flexDirection='row' alignItems='flex-end'>
+                  <Item>
+                    <Box display='flex' alignItems='flex-end'>
+                      <PersonOutlineOutlinedIcon
+                        style={{ fontSize: '1rem' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        Peter Giroud -
+                      </Link>
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <AccessTimeOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      &nbsp;April 5, 2018 -
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <FolderOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        &nbsp;Browse Our Sales
+                      </Link>
+                    </Box>
+                  </Item>
+                </Box>
+                <Link
+                  sx={{
+                    '&:hover': { color: '#f03637', transition: 'all 500ms' },
+                  }}
+                  variant='body1'
+                  fontWeight={500}
+                  underline='none'
+                  key='1'
+                  href='#'
+                >
+                  Sample Post With Format Audio
+                </Link>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Card>
+                <Link href='#'>
+                  <CardMedia
+                    component='img'
+                    image={`${IMAGES.blog1}`}
+                    alt='On Salvatore Ferragamo’s New Chapter'
+                  />
+                </Link>
+                <Box display='flex' flexDirection='row' alignItems='flex-end'>
+                  <Item>
+                    <Box display='flex' alignItems='flex-end'>
+                      <PersonOutlineOutlinedIcon
+                        style={{ fontSize: '1rem' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        Peter Giroud -
+                      </Link>
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <AccessTimeOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      &nbsp;February 8, 2018 -
+                    </Box>
+                  </Item>
+                  <Item>
+                    <Box display='flex'>
+                      &nbsp;
+                      <FolderOutlinedIcon
+                        style={{ fontSize: '13px' }}
+                        color='secondary'
+                      />
+                      <Link
+                        underline='none'
+                        href='#'
+                        color='secondary'
+                        sx={{
+                          '&:hover': {
+                            color: '#f03637',
+                            transition: 'all 500ms',
+                          },
+                        }}
+                      >
+                        &nbsp;Newest Items
+                      </Link>
+                    </Box>
+                  </Item>
+                </Box>
+                <Link
+                  sx={{
+                    '&:hover': { color: '#f03637', transition: 'all 500ms' },
+                  }}
+                  variant='body1'
+                  fontWeight={500}
+                  underline='none'
+                  key='1'
+                  href='#'
+                >
+                  Sample Post With Format Image
+                </Link>
+              </Card>
+            </Grid>
+          </Grid>
         </Grid>
+        <Grid item md={3}></Grid>
       </Grid>
     </>
   );
