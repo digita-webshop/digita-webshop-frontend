f0344ea16fe4394cccdb2daeb66da61325e2f4ca
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-06-16 22:47:07 +0430

    feat(article):add description & complete the first article

--- src/Pages/Main/Blog/Blog.tsx
+++ src/Pages/Main/Blog/Blog.tsx
@@ -4,7 +4,12 @@ import { Grid, Typography } from '@mui/material';
 import IMAGES from '../../../Assets/Images';
 import CustomSeparator from '../../../Components/CustomSeparator/CustomSeparator';
 import Link from '@mui/material/Link';
-
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+import FolderOutlinedIcon from '@mui/icons-material/FolderOutlined';
+import Box from '@mui/material/Box';
 
 const bgStyles = {
   display: 'flex',
@@ -23,6 +28,15 @@ const bgStyles = {
     xs: 5,
   },
 };
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  paddingTop: theme.spacing(2),
+  paddingBottom: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  fontSize: 10,
+}));
 
 function Blog() {
   return (
@@ -47,6 +61,67 @@ function Blog() {
                 image={`${IMAGES.blog6}`}
                 alt='On Salvatore Ferragamo’s New Chapter'
               />
+            </Link>
+            <Box display='flex' flexDirection='row' alignItems='flex-end'>
+              <Item>
+                <Box display='flex' alignItems='flex-end'>
+                  <PersonOutlineOutlinedIcon
+                    style={{ fontSize: '1rem' }}
+                    color='secondary'
+                  />
+                  <Link
+                    underline='none'
+                    href='#'
+                    color='secondary'
+                    sx={{
+                      '&:hover': { color: '#f03637', transition: 'all 500ms' },
+                    }}
+                  >
+                    Peter Giroud -
+                  </Link>
+                </Box>
+              </Item>
+              <Item>
+                <Box display='flex'>
+                  &nbsp;
+                  <AccessTimeOutlinedIcon
+                    style={{ fontSize: '13px' }}
+                    color='secondary'
+                  />
+                  &nbsp;July 9, 2019 -
+                </Box>
+              </Item>
+              <Item>
+                <Box display='flex'>
+                  &nbsp;
+                  <FolderOutlinedIcon
+                    style={{ fontSize: '13px' }}
+                    color='secondary'
+                  />
+                  <Link
+                    underline='none'
+                    href='#'
+                    color='secondary'
+                    sx={{
+                      '&:hover': { color: '#f03637', transition: 'all 500ms' },
+                    }}
+                  >
+                    &nbsp;Browse Our Sales
+                  </Link>
+                </Box>
+              </Item>
+            </Box>
+            <Link
+              sx={{
+                '&:hover': { color: '#f03637', transition: 'all 500ms' },
+              }}
+              variant='body2'
+              fontWeight={500}
+              underline='none'
+              key='1'
+              href='#'
+            >
+              On Salvatore Ferragamo’s New Chapter
             </Link>
           </Card>
         </Grid>
