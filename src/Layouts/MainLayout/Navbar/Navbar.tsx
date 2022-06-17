5d60906a4ecbedcd066b639c73a1f1c9a1c44c6f
tohid <tohidebrahimi17@gmail.com>
2022-06-17 21:40:14 +0430

    feat(navbar):add scroll animation

--- src/Layouts/MainLayout/Navbar/Navbar.tsx
+++ src/Layouts/MainLayout/Navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import {
   useMediaQuery,
   Fade,
   SelectChangeEvent,
+  Collapse,
 } from "@mui/material";
 import logoImg from "../../../Assets/Images/digita-logo.png";
 import { navbarItems } from "../../../Services/Utils/Data/data";
@@ -20,7 +21,7 @@ import {
   MenuRounded,
   KeyboardArrowDown,
 } from "@mui/icons-material";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { AntTab, AntTabs } from "../../../Styles/Appbar";
 import TabDrawer from "./TabDrawer/TabDrawer";
 import ShopDrawer from "./ShopDrawer/ShopDrawer";
@@ -40,6 +41,7 @@ function Navbar() {
   const [displayMenu, setDisplayMenu] = useState(false);
   const [displayShopMenu, setDisplayShopMenu] = useState(false);
   const [openSearchBar, setOpenSearchBar] = useState(false);
+  const [collapse, setCollapse] = useState(true);
 
   const [displayDrawer, setDisplayDrawer] = useState({
     left: false,
@@ -79,196 +81,223 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    let lastScroll = window.scrollY;
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 180 && lastScroll < window.scrollY) {
+        setCollapse(false);
+      } else {
+        setCollapse(true);
+      }
+      lastScroll = window.scrollY;
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
   return (
-    <AppBar sx={{ backgroundColor: "white", position: "relative" }}>
-      <Container
-        maxWidth={"xl"}
+    <Fragment>
+      <AppBar
         sx={{
-          position: "relative",
+          backgroundColor: "white",
+          position: "fixed ",
         }}
-        onMouseEnter={() => displayMenuHandler(false)}
       >
-        <Toolbar
-          sx={{
-            justifyContent: "space-between",
-          }}
-          disableGutters
-        >
-          <Box>
-            <Box onClick={() => toggleDrawer("left", true)}>
-              <MenuRounded
-                fontSize={"large"}
-                color="primary"
-                sx={{
-                  display: {
-                    sx: "block",
-                    md: "none",
-                    cursor: "pointer",
-                    "&:hover": { color: "#f03637" },
-                  },
-                }}
-              />
-            </Box>
-            <TabDrawer
-              displayDrawer={displayDrawer}
-              toggleDrawer={toggleDrawer}
-            />
-          </Box>
-          <Box sx={{ marginRight: { sx: "0", md: "30px" } }}>
-            <img src={logoImg} alt="digita-logo" />
-          </Box>
-
-          {!openSearchBar && (
-            <Fragment>
-              <AntTabs value={pathname}>
-                {navbarItems.map((item) => (
-                  <AntTab
-                    key={item.id}
-                    label={item.name}
-                    component={Link}
-                    icon={item.name === "shop" && <KeyboardArrowDown />}
-                    iconPosition="right"
-                    value={`${item.route}`}
-                    to={`${item.route}`}
-                    onMouseEnter={() => showShopMenuHandler(item.name)}
+        <Collapse in={collapse}>
+          <Container
+            maxWidth={"xl"}
+            sx={{
+              position: "relative",
+            }}
+            onMouseEnter={() => displayMenuHandler(false)}
+          >
+            <Toolbar
+              sx={{
+                justifyContent: "space-between",
+              }}
+              disableGutters
+            >
+              <Box>
+                <Box onClick={() => toggleDrawer("left", true)}>
+                  <MenuRounded
+                    fontSize={"large"}
+                    color="primary"
                     sx={{
-                      color:
-                        displayShopMenu && item.name === "shop"
-                          ? "#f03637"
-                          : "",
-                      borderBottom:
-                        displayShopMenu && item.name === "shop"
-                          ? "3px solid #f03637"
-                          : "",
+                      display: {
+                        sx: "block",
+                        md: "none",
+                        cursor: "pointer",
+                        "&:hover": { color: "#f03637" },
+                      },
                     }}
                   />
-                ))}
-              </AntTabs>
-              <Box
-                sx={{
-                  marginLeft: { sx: "0", md: "auto" },
-                  height: { md: "90px" },
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <Box onClick={openSearchBarHandler}>
-                  <Badge
-                    showZero
-                    sx={{
-                      display: {
-                        xs: "none",
-                        md: "inline-flex",
-                      },
-                    }}
-                  >
-                    <SearchOutlined color="primary" sx={navbarIcons} />
-                  </Badge>
                 </Box>
-                <Badge
-                  showZero
-                  sx={{
-                    display: {
-                      xs: "none",
-                      md: "inline-flex",
-                    },
-                  }}
-                >
-                  <LoginOutlined color="primary" sx={navbarIcons} />
-                </Badge>
-                <Box
-                  display={"flex"}
-                  height={"100%"}
-                  onMouseEnter={() => displayMenuHandler(false)}
-                >
-                  <Badge
-                    badgeContent={4}
-                    overlap="circular"
-                    color="error"
-                    sx={{
-                      display: {
-                        xs: "none",
-                        md: "inline-flex",
-                      },
-                      margin: "auto",
-                    }}
-                  >
-                    <FavoriteBorderOutlined color="primary" sx={navbarIcons} />
-                  </Badge>
-                </Box>
-                <Box
-                  onMouseEnter={() => displayMenuHandler(true)}
-                  onClick={() => toggleDrawer("right", true)}
-                >
-                  <Badge
-                    badgeContent={4}
-                    overlap="circular"
-                    color="error"
-                    sx={{ margin: "auto" }}
-                  >
-                    <LocalGroceryStoreOutlined
-                      color="primary"
-                      sx={{
-                        marginLeft: "12px",
-                        transition: "all 200ms",
-                        cursor: "pointer",
-                        fontSize: "28px",
-                        "&:hover": { color: "#f03637" },
-                      }}
-                    />
-                  </Badge>
-                </Box>
-                <ShopDrawer
+                <TabDrawer
                   displayDrawer={displayDrawer}
                   toggleDrawer={toggleDrawer}
                 />
               </Box>
-            </Fragment>
-          )}
+              <Box sx={{ marginRight: { sx: "0", md: "30px" } }}>
+                <img src={logoImg} alt="digita-logo" />
+              </Box>
+
+              {!openSearchBar && (
+                <Fragment>
+                  <AntTabs value={pathname}>
+                    {navbarItems.map((item) => (
+                      <AntTab
+                        key={item.id}
+                        label={item.name}
+                        component={Link}
+                        icon={item.name === "shop" && <KeyboardArrowDown />}
+                        iconPosition="right"
+                        value={`${item.route}`}
+                        to={`${item.route}`}
+                        onMouseEnter={() => showShopMenuHandler(item.name)}
+                        sx={{
+                          color:
+                            displayShopMenu && item.name === "shop"
+                              ? "#f03637"
+                              : "",
+                          borderBottom:
+                            displayShopMenu && item.name === "shop"
+                              ? "3px solid #f03637"
+                              : "",
+                        }}
+                      />
+                    ))}
+                  </AntTabs>
+                  <Box
+                    sx={{
+                      marginLeft: { sx: "0", md: "auto" },
+                      height: { md: "90px" },
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Box onClick={openSearchBarHandler}>
+                      <Badge
+                        showZero
+                        sx={{
+                          display: {
+                            xs: "none",
+                            md: "inline-flex",
+                          },
+                        }}
+                      >
+                        <SearchOutlined color="primary" sx={navbarIcons} />
+                      </Badge>
+                    </Box>
+                    <Badge
+                      showZero
+                      sx={{
+                        display: {
+                          xs: "none",
+                          md: "inline-flex",
+                        },
+                      }}
+                    >
+                      <LoginOutlined color="primary" sx={navbarIcons} />
+                    </Badge>
+                    <Box
+                      display={"flex"}
+                      height={"100%"}
+                      onMouseEnter={() => displayMenuHandler(false)}
+                    >
+                      <Badge
+                        badgeContent={4}
+                        overlap="circular"
+                        color="error"
+                        sx={{
+                          display: {
+                            xs: "none",
+                            md: "inline-flex",
+                          },
+                          margin: "auto",
+                        }}
+                      >
+                        <FavoriteBorderOutlined
+                          color="primary"
+                          sx={navbarIcons}
+                        />
+                      </Badge>
+                    </Box>
+                    <Box
+                      onMouseEnter={() => displayMenuHandler(true)}
+                      onClick={() => toggleDrawer("right", true)}
+                    >
+                      <Badge
+                        badgeContent={4}
+                        overlap="circular"
+                        color="error"
+                        sx={{ margin: "auto" }}
+                      >
+                        <LocalGroceryStoreOutlined
+                          color="primary"
+                          sx={{
+                            marginLeft: "12px",
+                            transition: "all 200ms",
+                            cursor: "pointer",
+                            fontSize: "28px",
+                            "&:hover": { color: "#f03637" },
+                          }}
+                        />
+                      </Badge>
+                    </Box>
+                    <ShopDrawer
+                      displayDrawer={displayDrawer}
+                      toggleDrawer={toggleDrawer}
+                    />
+                  </Box>
+                </Fragment>
+              )}
+              <Fade
+                style={{ display: openSearchBar ? "block" : "none" }}
+                in={openSearchBar}
+              >
+                <Box
+                  sx={{
+                    width: "100%",
+                  }}
+                >
+                  <SearchBar
+                    selectedCategory={selectedCategory}
+                    selectedCategoryHandler={selectedCategoryHandler}
+                    openSearchBarHandler={openSearchBarHandler}
+                  />
+                </Box>
+              </Fade>
+            </Toolbar>
+            <ShopMenuCard
+              displayMenu={displayMenu}
+              displayMenuHandler={displayMenuHandler}
+            />
+          </Container>
           <Fade
-            style={{ display: openSearchBar ? "block" : "none" }}
-            in={openSearchBar}
+            style={{ display: displayShopMenu ? "block" : "none" }}
+            in={displayShopMenu}
           >
             <Box
+              paddingY={3}
+              paddingX={1}
               sx={{
-                width: "100%",
+                border: "1px solid #dedede",
+                boxSizing: "border-box",
+                backgroundColor: "white",
+                position: "absolute",
+                left: 0,
+                right: 0,
+                top: 90,
               }}
+              onMouseLeave={() => setDisplayShopMenu(false)}
             >
-              <SearchBar
-                selectedCategory={selectedCategory}
-                selectedCategoryHandler={selectedCategoryHandler}
-                openSearchBarHandler={openSearchBarHandler}
-              />
+              <ShopMenu />
             </Box>
           </Fade>
-        </Toolbar>
-        <ShopMenuCard
-          displayMenu={displayMenu}
-          displayMenuHandler={displayMenuHandler}
-        />
-      </Container>
-      <Fade
-        style={{ display: displayShopMenu ? "block" : "none" }}
-        in={displayShopMenu}
-      >
-        <Box
-          paddingY={3}
-          paddingX={1}
-          sx={{
-            border: "1px solid #dedede",
-            boxSizing: "border-box",
-            backgroundColor: "white",
-            position: "absolute",
-            left: 0,
-            right: 0,
-            top: 90,
-          }}
-          onMouseLeave={() => setDisplayShopMenu(false)}
-        >
-          <ShopMenu />
-        </Box>
-      </Fade>
-    </AppBar>
+        </Collapse>
+      </AppBar>
+      <Box sx={{ marginTop: { xs: "64px", md: "90px" } }}></Box>
+    </Fragment>
   );
 }
 
