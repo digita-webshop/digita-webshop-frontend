2626a294aef3f2c692bcd0394160541e5df83d18
Siavash <siavash.khani200@gmail.com>
2022-06-17 19:24:54 +0430

    feat(team & teammember): components added and it's fully dynamic

--- src/Services/Utils/Data/data.ts
+++ src/Services/Utils/Data/data.ts
@@ -130,3 +130,11 @@ export const specialProducts = [
     {id: 8, name: 'SIRUM 360 Holder...', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2020/11/product-8-330x330.jpg',offPrice: 0, price: 12, starRate: 5},
     {id: 9, name: 'SmartPhone & Ipad', image: 'https://demo-61.woovinapro.com/wp-content/uploads/2020/11/product-3-330x330.jpg',offPrice: 0, price: 75, starRate: 5},
 ]
+
+export const teamData = [
+    {id: 1, name: "Francis Zimmerman", image: "https://demo-61.woovinapro.com/wp-content/uploads/2022/04/team04.png", job: "CEO & Founder"},
+    {id: 2, name: "Timmy Morrison", image: "https://demo-61.woovinapro.com/wp-content/uploads/2022/04/team03.png", job: "Marketing"},
+    {id: 3, name: "Crystal Buchanan", image: "https://demo-61.woovinapro.com/wp-content/uploads/2022/04/team01.png", job: "Tech Leader"},
+    {id: 4, name: "Darrel French", image: "https://demo-61.woovinapro.com/wp-content/uploads/2022/04/team05.png", job: "Manager"},
+    {id: 5, name: "Gabriel Barker", image: "https://demo-61.woovinapro.com/wp-content/uploads/2022/04/team02.png", job: "Designer"},
+]
