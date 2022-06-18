f916bda3b788557edee67db0ba9958d07db8c6ac
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-06-18 20:55:07 +0430

    refactor(side bar): update search bar

--- src/Components/BlogPage/Components/SideBar/SearchBar/SearchBar.tsx
+++ src/Components/BlogPage/Components/SideBar/SearchBar/SearchBar.tsx
@@ -4,40 +4,40 @@ import InputBase from '@mui/material/InputBase';
 import { Typography } from '@mui/material';
 
 const SearchBar = () => {
-    return (
-        <Box
+  return (
+    <Box
+      sx={{
+        border: '1px solid #e9e9e9',
+        px: '20px',
+        pt: '20px',
+        pb: '30px',
+        mb: '30px',
+      }}
+    >
+      <Typography
+        variant='subtitle2'
         sx={{
-          border: '1px solid #e9e9e9',
-          px: '20px',
-          pt: '20px',
-          pb: '30px',
-          mb: '30px',
+          pb: '20px',
         }}
       >
-        <Typography
-          variant='subtitle2'
-          sx={{
-            pb: '20px',
-          }}
-        >
-          SEARCH
-        </Typography>
-        <Box sx={{ width: 40, borderBottom: '3px solid #f03637' }}></Box>
-        <Divider sx={{ mb: '14px' }} />
-        <InputBase
-          sx={{
-            backgroundColor: '#e9e9e9',
-            borderRadius: '4px',
-            py: '6px',
-            px: '12px',
-            fontSize: '14px',
-            width: '100%',
-          }}
-          placeholder='Search'
-          inputProps={{ 'aria-label': 'search' }}
-        />
-      </Box>
-    )
-}
+        SEARCH
+      </Typography>
+      <Box sx={{ width: 40, borderBottom: '3px solid #f03637' }}></Box>
+      <Divider sx={{ mb: '14px' }} />
+      <InputBase
+        sx={{
+          backgroundColor: '#e9e9e9',
+          borderRadius: '4px',
+          py: '6px',
+          px: '12px',
+          fontSize: '14px',
+          width: '100%',
+        }}
+        placeholder='Search'
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </Box>
+  );
+};
 
 export default SearchBar;
