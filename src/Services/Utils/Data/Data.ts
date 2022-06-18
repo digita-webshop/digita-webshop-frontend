cc6d2883ecdab0a594a9cc13ac19a941c8e05496
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-06-18 12:24:00 +0430

    refactor(articles):add article information to data file

--- src/Services/Utils/Data/Data.ts
+++ src/Services/Utils/Data/Data.ts
@@ -1,27 +1,80 @@
-export const navbarItems=[
-    {
-        id:1,
-        name:'home',
-        route:'/',
-    },
-    {
-        id:2,
-        name:'shop',
-        route:'/shop',
-    },
-    {
-        id:3,
-        name:'blog',
-        route:'/blog',
-    },
-    {
-        id:4,
-        name:'contact us',
-        route:'/contact-us',
-    },
-    {
-        id:5,
-        name:'about us',
-        route:'/about-us',
-    }
-]
+import IMAGES from '../../../Assets/Images';
+
+export const navbarItems = [
+  {
+    id: 1,
+    name: 'home',
+    route: '/',
+  },
+  {
+    id: 2,
+    name: 'shop',
+    route: '/shop',
+  },
+  {
+    id: 3,
+    name: 'blog',
+    route: '/blog',
+  },
+  {
+    id: 4,
+    name: 'contact us',
+    route: '/contact-us',
+  },
+  {
+    id: 5,
+    name: 'about us',
+    route: '/about-us',
+  },
+];
+
+export const articlesBlogPage = [
+  {
+    id: 1,
+    title: 'On Salvatore Ferragamo’s New Chapter',
+    image: `${IMAGES.blog6}`,
+    author: 'Peter Giroud',
+    releaseDate: 'July 9, 2019',
+    status: 'Browse Our Sales',
+  },
+  {
+    id: 2,
+    title: 'Decoding #OOTD Pose In The Modeling Industry',
+    image: `${IMAGES.blog5}`,
+    author: 'Peter Giroud',
+    releaseDate: 'October 5, 2018',
+    status: 'Men',
+  },
+  {
+    id: 3,
+    title: '8 Fashion Photographers In UK',
+    image: `${IMAGES.blog4}`,
+    author: 'Peter Giroud',
+    releaseDate: 'June 10, 2018',
+    status: 'On Sale',
+  },
+  {
+    id: 4,
+    title: 'Sample Post With Format Chat',
+    image: `${IMAGES.blog3}`,
+    author: 'Peter Giroud',
+    releaseDate: 'June 8, 2018',
+    status: 'Women',
+  },
+  {
+    id: 5,
+    title: 'Sample Post With Format Audio',
+    image: `${IMAGES.blog2}`,
+    author: 'Peter Giroud',
+    releaseDate: 'April 5, 2018',
+    status: 'Browse Our Sales',
+  },
+  {
+    id: 6,
+    title: 'Sample Post With Format Image',
+    image: `${IMAGES.blog1}`,
+    author: 'Peter Giroud',
+    releaseDate: 'February 8, 2018',
+    status: 'Newest Items',
+  },
+];
