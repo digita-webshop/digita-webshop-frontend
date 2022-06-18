31c0d2b826ab8d0676b0e02de4a83fb766a056d9
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-06-18 18:44:17 +0430

    refactor(article):update the article component

--- src/Components/BlogPage/Components/Articles/Article/ArticleCard.tsx
+++ src/Components/BlogPage/Components/Articles/Article/ArticleCard.tsx
@@ -3,7 +3,6 @@ import CardTitle from './Components/CardTitle/CardTitle';
 import CardDescription from './Components/CardDescription/CardDescription';
 import Card from '@mui/material/Card';
 import { Grid } from '@mui/material';
-
 interface info {
   title: string;
   image: string;
@@ -16,15 +15,12 @@ function ArticleCard(props: info) {
   return (
     <Grid item xs={12} sm={6} px={'15px'} mb={'35px'}>
       <Card>
-        {/* card image */}
         <CardImage title={props.title} image={props.image}></CardImage>
-        {/* card description */}
         <CardDescription
           author={props.author}
           date={props.releaseDate}
           status={props.status}
         ></CardDescription>
-        {/* card title */}
         <CardTitle title={props.title}></CardTitle>
       </Card>
     </Grid>
