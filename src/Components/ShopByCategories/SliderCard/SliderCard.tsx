ba71f47d08aaaa3e7b365d43bfdf0bfddb48f8f0
monire <aglida1370@gmail.com>
2022-06-18 15:56:50 +0430

    refactor(shopByCat):refactored code

--- src/Components/ShopByCategories/SliderCard/SliderCard.tsx
+++ src/Components/ShopByCategories/SliderCard/SliderCard.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import {styled} from "@mui/material/styles";
-import {Box, Typography} from "@mui/material";
-import img1 from "../../../Assets/Images/ShopByCat/cate1.jpg"
+import {Box} from "@mui/material";
+import Item from "./Types/Item";
+import {Name} from './Styles'
 
 type Props = {
-    item: {
-        id: number,
-        name: string,
-        img: string
-    }
+    item: Item
 };
 
 const SliderCard = ({item}: Props) => {
 
-
     const Card = styled(Box)(({theme}) => ({
-        width: '270px',
+        width: '100%',
         position: 'relative',
         img: {
             maxWidth: '100%',
@@ -25,15 +21,7 @@ const SliderCard = ({item}: Props) => {
     return (
         <Card>
             <img src={item.img}/>
-            <Typography sx={{
-                fontWeight: 600,
-                textTransform: 'uppercase',
-                fontSize: '14px',
-                position: 'absolute',
-                bottom: '30px',
-                left: '50%',
-                transform: 'translateX(-50%)'
-            }}>{item.name}</Typography>
+            <Name>{item.name}</Name>
         </Card>
     );
 };
