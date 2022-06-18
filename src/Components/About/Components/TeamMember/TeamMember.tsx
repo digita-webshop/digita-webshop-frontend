ae5f436b736fad2b687ca8f03ce3d465c04b1e1f
Siavash Khani <siavash.khani200@gmail.com>
2022-06-18 15:34:52 +0430

    style(about): component poster and contents styles are now responsive and fixed

--- src/Components/About/Components/TeamMember/TeamMember.tsx
+++ src/Components/About/Components/TeamMember/TeamMember.tsx
@@ -1,6 +1,5 @@
 import { Grid, Box, Typography } from "@mui/material";
 
-
 interface T {
   image: string;
   name: string;
@@ -15,14 +14,20 @@ const itemStyle = {
   gap: 1,
 };
 
-const TeamMember = ({image, name, job}: T) => {
+const iconStyle = {
+
+}
+
+const TeamMember = ({ image, name, job }: T) => {
   return (
     <Grid item xs={6} sm={6} md={4} lg={2.4}>
       <Box sx={itemStyle}>
-        <Box
-          component="img"
-          src={image}
-        />
+        <Box>
+          <Box component="img" src={image} />
+          <Box sx={iconStyle}>
+              icons
+          </Box>
+        </Box>
         <Typography
           sx={{
             fontWeight: 700,
@@ -34,9 +39,7 @@ const TeamMember = ({image, name, job}: T) => {
         >
           {name}
         </Typography>
-        <Typography sx={{ fontWeight: 400, color: "#777" }}>
-          {job}
-        </Typography>
+        <Typography sx={{ fontWeight: 400, color: "#777" }}>{job}</Typography>
       </Box>
     </Grid>
   );
