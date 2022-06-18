548bfa08f99dc2e658a126d0560075df7aad5686
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-06-18 20:57:52 +0430

    refactor(Blog):add BlogPage & update Blog component

---
+++ src/Components/BlogPage/BlogPage.tsx
@@ -0,0 +1,27 @@
+import { Grid } from '@mui/material';
+import Header from './Components/Header/Header';
+import Articles from './Components/Articles/Articles';
+import SearchBar from './Components/SideBar/SearchBar/SearchBar';
+import RecentPosts from './Components/SideBar/RecentPosts/RecentPosts';
+import RecentComments from './Components/SideBar/RecentComments/RecentComments';
+
+function BlogPage() {
+  return (
+    <>
+      <Header></Header>
+      <Grid
+        container
+        sx={{ py: 6.25, px: '15px', maxWidth: '1200px', mx: 'auto', my: 0 }}
+      >
+        <Articles></Articles>
+        <Grid item xs={12} md={3}>
+          <SearchBar></SearchBar>
+          <RecentPosts></RecentPosts>
+          <RecentComments></RecentComments>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default BlogPage;
