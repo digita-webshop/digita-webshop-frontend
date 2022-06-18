d9b6cd2c0b76e04430d2f6845fe38c52e1bc3675
Siavash Khani <siavash.khani200@gmail.com>
2022-06-18 18:23:09 +0430

    fix(content): typography bug is fixed

--- src/Components/About/Components/Team/Team.tsx
+++ src/Components/About/Components/Team/Team.tsx
@@ -1,24 +1,12 @@
 import { Grid, Box, Typography } from "@mui/material";
 import { teamData } from "../../../../Services/Utils/Data/data";
 import TeamMember from "../TeamMember/TeamMember";
-
-const titleStyle = {
-  padding: "8rem 0 1rem 0",
-  color: "#333",
-  fontSize: "32px",
-  fontWeight: "600",
-};
-
-export const teamsStyle = {
-  display: "flex",
-  alignItems: "center",
-  width: "100%",
-};
+import {teamTitleStyle, teamsStyle} from "../../../../Styles/About";
 
 const Team = () => {
   return (
     <Box sx={{ textAlign: "center", mb: 8 }}>
-      <Typography variant="h4" sx={titleStyle}>
+      <Typography variant="h4" sx={teamTitleStyle}>
         OUR TEAM
       </Typography>
       <Grid container sx={teamsStyle}>
--- src/Components/About/Components/TeamMember/TeamMember.tsx
+++ src/Components/About/Components/TeamMember/TeamMember.tsx
@@ -2,6 +2,12 @@ import { Grid, Box, Typography } from "@mui/material";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GoogleIcon from "@mui/icons-material/Google";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import {
+  avatarWrapper,
+  teamItemStyle,
+  iconStyle,
+  iconCircle,
+} from "../../../../Styles/About";
 
 interface T {
   image: string;
@@ -9,65 +15,23 @@ interface T {
   job: string;
 }
 
-const avatarWrapper = {
-  position: "relative",
-  "&:hover": {
-    "& .imgBox": {
-      opacity: "0.6",
-      zIndex: "-1",
-    },
-    "& .iconBox": {
-      display: "flex",
-    }
-  }
-}
-
-const itemStyle = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  gap: 1,
-};
-
-const iconStyle = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%,-50%)",
-  display: "none",
-  zIndex: "1",
-};
-
-const iconCircle = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  width: "32px",
-  height: "32px",
-  borderRadius: "50%",
-  backgroundColor: "#fff",
-  mr: 1,
-  cursor: "pointer",
-};
-
 const TeamMember = ({ image, name, job }: T) => {
   return (
     <Grid item xs={6} sm={6} md={4} lg={2.4}>
-      <Box sx={itemStyle}>
+      <Box sx={teamItemStyle}>
         <Box sx={avatarWrapper}>
           <Box sx={iconStyle} className="iconBox">
             <Box sx={iconCircle}>
-              <TwitterIcon sx={{ color: "#3498db"}} />
+              <TwitterIcon sx={{ color: "#3498db" }} />
             </Box>
             <Box sx={iconCircle}>
-              <GoogleIcon sx={{ color: "#E71140"}} />
+              <GoogleIcon sx={{ color: "#E71140" }} />
             </Box>
             <Box sx={iconCircle}>
-              <GitHubIcon sx={{ color: "#333"}} />
+              <GitHubIcon sx={{ color: "#333" }} />
             </Box>
           </Box>
-          <Box component="img" src={image} className="imgBox"/>
+          <Box component="img" src={image} className="imgBox" />
         </Box>
         <Typography
           sx={{
