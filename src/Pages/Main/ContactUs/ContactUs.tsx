2ae79665d3e89be0ff19c7a8172879a3c31be218
tohid <tohidebrahimi17@gmail.com>
2022-06-19 16:21:42 +0430

    feat(contactUs):add contact form ui

--- src/Pages/Main/ContactUs/ContactUs.tsx
+++ src/Pages/Main/ContactUs/ContactUs.tsx
@@ -1,5 +1,10 @@
+import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
-import { ContactUsAddress, CustomBreadcrumbs } from "../../../Components";
+import {
+  ContactUsAddress,
+  ContactUsForm,
+  CustomBreadcrumbs,
+} from "../../../Components";
 
 function ContactUs() {
   return (
@@ -7,6 +12,12 @@ function ContactUs() {
       <CustomBreadcrumbs title={"contact us "} />
       <Container maxWidth={"lg"}>
         <ContactUsAddress />
+        <Grid container spacing={3} marginY={6}>
+          <Grid item xs={12} md={6}></Grid>
+          <Grid item xs={12} md={6}>
+            <ContactUsForm />
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
