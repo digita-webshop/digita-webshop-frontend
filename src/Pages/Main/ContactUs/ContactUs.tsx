8f0142434bc2b39c96400583fe87a5f70775233e
tohid <tohidebrahimi17@gmail.com>
2022-06-19 22:19:27 +0430

    feat(contactUs):add google map

--- src/Pages/Main/ContactUs/ContactUs.tsx
+++ src/Pages/Main/ContactUs/ContactUs.tsx
@@ -4,6 +4,7 @@ import {
   ContactUsAddress,
   ContactUsForm,
   CustomBreadcrumbs,
+  Map,
 } from "../../../Components";
 
 function ContactUs() {
@@ -13,7 +14,14 @@ function ContactUs() {
       <Container maxWidth={"lg"}>
         <ContactUsAddress />
         <Grid container spacing={3} marginY={6}>
-          <Grid item xs={12} md={6}></Grid>
+          <Grid item xs={12} md={6}>
+            <Map
+            // googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
+            // loadingElement={<div style={{ height: `100%` }} />}
+            // containerElement={<div style={{ height: `400px` }} />}
+            // mapElement={<div style={{ height: `100%` }} />}
+            />
+          </Grid>
           <Grid item xs={12} md={6}>
             <ContactUsForm />
           </Grid>
