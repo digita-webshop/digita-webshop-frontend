70050950c85bad859cdfb879bf180503b4074e5f
monire <aglida1370@gmail.com>
2022-06-20 15:41:51 +0430

    refactor(whatClientSay):code refactor

--- src/Components/WhatClientSay/ClientAvatar/ClientAvatar.tsx
+++ src/Components/WhatClientSay/ClientAvatar/ClientAvatar.tsx
@@ -1,61 +1,62 @@
 import React from "react";
-import { Box, Grid, Rating, Typography } from "@mui/material";
+import {Box, Grid, Rating, Typography} from "@mui/material";
+import { AvatarProps} from "../ClientCard/Types/Item";
 
-interface AvatarProps {
-  profileImg: string;
-  username: string;
-  role: string;
-  rate: number;
+type Props = {
+    user: AvatarProps
 }
 
-function ClientAvatar({ profileImg, username, role, rate }: AvatarProps) {
-  return (
-    <>
-      <Box sx={{ mt: 3, "& img": { width: 70, height: 70 } }}>
-        <Grid container spacing={1}>
-          <Grid item>
-            <img src={profileImg} alt="profile" />
-          </Grid>
-          <Grid item>
-            <Grid
-              container
-              direction="column"
-              spacing={0}
-              justifyContent="space-between"
-              alignItems="flex-start"
-            >
-              <Grid item>
-                <Typography
-                  sx={{
-                    fontSize: 15,
-                    textTransform: "uppercase",
-                    fontWeight: "600",
-                  }}
-                >
-                  {username}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography
-                  sx={{
-                    fontSize: 13,
-                    textTransform: "uppercase",
-                    fontWeight: "600",
-                    mb: 1,
-                  }}
-                >
-                  {role}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Rating size="small" name="read-only" value={rate} readOnly />
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Box>
-    </>
-  );
+function ClientAvatar({user}: Props) {
+    return (
+        <>
+            <Box sx={{mt: 3, "& img": {width: 70, height: 70}}}>
+                <Grid container spacing={1}>
+                    <Grid item>
+                        <img src={user.profileImg} alt="profile"/>
+                    </Grid>
+                    <Grid item>
+                        <Grid
+                            sx={{
+
+                                ml: '6px',
+                            }}
+                            container
+                            direction="column"
+                            spacing={0}
+                            justifyContent="space-between"
+                            alignItems="flex-start"
+                        >
+                            <Grid item>
+                                <Typography
+                                    sx={{
+                                        fontSize: 15,
+                                        textTransform: "uppercase",
+                                        fontWeight: "600",
+                                    }}
+                                >
+                                    {user.username}
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography
+                                    sx={{
+                                        fontSize: 15,
+                                        fontWeight: "400",
+                                        mb: 1,
+                                    }}
+                                >
+                                    {user.role}
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Rating size="small" name="read-only" value={user.rate} readOnly/>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Box>
+        </>
+    );
 }
 
 export default ClientAvatar;
