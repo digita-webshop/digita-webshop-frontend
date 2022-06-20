9107cdeaf54be21c46766fca23bc1d660cb8866b
tohid <tohidebrahimi17@gmail.com>
2022-06-20 11:37:56 +0430

    refactor(contactUs):refactored map component

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,53 +1,32 @@
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-import { useRef } from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 function Map() {
   const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: "AIzaSyDsp8HGkj_cvdYpARwETEjlfr63NVGWysU",
+    googleMapsApiKey: `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`,
   });
-  const mapRef = useRef(null);
-  const onLoad = (map: any): void => {
-    mapRef.current = map;
-  };
 
-  const onUnMount = (): void => {
-    mapRef.current = null;
-  };
   const containerStyle = {
     width: "100%",
     height: "600px",
   };
 
-  // Center on Kalmar
-  const center = {
-    lat: 56.68,
-    lng: 16.33,
-  };
+  const center = { lat: 51.5, lng: -0.12 };
 
-  // Disable default UI
   const options = {
-    // styles: mapStyles,
-    disableDefaultUI: true,
     zoomControl: true,
   };
 
   if (!isLoaded) return <div>Map Loading ...</div>;
   return (
     <GoogleMap
-      // zoom={8}
-      // defaultCenter={{ lat: -34.397, lng: 150.644 }}
       mapContainerStyle={containerStyle}
       options={options as google.maps.MapOptions}
       center={center}
       zoom={12}
-      onLoad={onLoad}
-      onUnmount={onUnMount}
-    />
-    //   {/* <Marker position={{ lat: -34.397, lng: 150.644 }} /> */}
-    // </GoogleMap>
+    >
+      <Marker position={{ lat: 51.5, lng: -0.12 }} />
+    </GoogleMap>
   );
 }
 
 export default Map;
-// export default withScriptjs(withGoogleMap(Map));
