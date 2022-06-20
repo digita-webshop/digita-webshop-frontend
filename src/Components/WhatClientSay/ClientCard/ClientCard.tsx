97b2851f4afc26b3bf759866e84f8d1ddb19888c
monire <aglida1370@gmail.com>
2022-06-20 15:42:11 +0430

    refactor(whatClientSay):code refactor and passed dynamic props

--- src/Components/WhatClientSay/ClientCard/ClientCard.tsx
+++ src/Components/WhatClientSay/ClientCard/ClientCard.tsx
@@ -1,40 +1,41 @@
-import { Box } from "@mui/material";
+import {Box} from "@mui/material";
 import React from "react";
-import { ReusableTypography } from "../../../Styles/WhatClientSay";
+import {ReusableTypography} from "../../../Styles/WhatClientSay";
 import ClientAvatar from "../ClientAvatar/ClientAvatar";
+import {ItemType} from "./Types/Item";
 
-import iconTitle from "../../../Assets/Images/WhatClientSayAssets/icon-title.png";
-import profileImg from "../../../Assets/Images/WhatClientSayAssets/testimonial-2.png";
+type Props = {
+    item: ItemType
+}
 
-function ClientCard(props: any) {
-  return (
-    <>
-      <ReusableTypography
-        fontWeight="400"
-        smallSizeFontSize="17px"
-        mediumSizeFontSize="24px"
-        component="p"
-        sx={{
-          fontSize: 32,
-          textAlign: "center",
-        }}
-      >
-        “Excepteur sin occaecat cupidatat proident. It’s sunt in culpa qui
-        officia deserunt mollit anim id est consectetur adipiscing eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut culpa qui officia
-        deserunt mollit enim ad minim veniam”
-      </ReusableTypography>
+function ClientCard({item}: Props) {
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: "center",
+            color: 'white',
+            position: 'relative',
+            width: '100%',
+        }}>
+            <ReusableTypography
+                fontWeight="400"
+                smallSizeFontSize="17px"
+                mediumSizeFontSize="24px"
+                component="p"
+                sx={{
+                    fontSize: 32,
+                    textAlign: "center",
+                }}
+            >
+                {item.text}
+            </ReusableTypography>
 
-      <Box sx={{ display: "grid", placeItems: "center" }}>
-        <ClientAvatar
-          username="naomi hamilton"
-          role="customer"
-          rate={5}
-          profileImg={profileImg}
-        />
-      </Box>
-    </>
-  );
+            <Box sx={{display: "grid", placeItems: "center"}}>
+                <ClientAvatar user={item.user}/>
+            </Box>
+        </Box>
+    );
 }
 
 export default ClientCard;
