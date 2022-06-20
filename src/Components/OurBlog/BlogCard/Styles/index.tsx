e09e056250720f568f9f07b217ee4f1904bfafa0
monire <aglida1370@gmail.com>
2022-06-20 18:41:43 +0430

    feat(ourBlog):added card styles

---
+++ src/Components/OurBlog/BlogCard/Styles/index.tsx
@@ -0,0 +1,13 @@
+import {styled} from "@mui/material/styles";
+import {Box} from "@mui/material";
+
+export const Card = styled(Box)(({theme}) => ({
+    width: '100%',
+    position: 'relative',
+    img: {
+        maxWidth: '100%',
+        height: 'auto'
+    }
+}));
+export const textStyle = {fontSize: '15px', color: '#777777'}
+export const iconStyle = {fontSize: '1rem', marginRight: '5px', color: '#777777'}
