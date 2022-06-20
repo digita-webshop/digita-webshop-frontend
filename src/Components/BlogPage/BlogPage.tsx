7c48eb77f9a6adbfb0405cd1023f18a35a8ae895
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-06-20 16:55:45 +0200

    feat(pagination):add pagination to blog page

--- src/Components/BlogPage/BlogPage.tsx
+++ src/Components/BlogPage/BlogPage.tsx
@@ -1,20 +1,51 @@
-import { Grid } from "@mui/material";
-import Articles from "./Components/Articles/Articles";
-import SearchBar from "./Components/SideBar/SearchBar/SearchBar";
-import RecentPosts from "./Components/SideBar/RecentPosts/RecentPosts";
-import RecentComments from "./Components/SideBar/RecentComments/RecentComments";
-import CustomBreadcrumbs from "../CustomBreadcrumbs/CustomBreadcrumbs";
+import { useState } from 'react'
+import Pagination from '../Pagination/Pagination'
+import { Grid } from '@mui/material'
+import ArticleCard from './Components/Articles/Article/ArticleCard'
+import SearchBar from './Components/SideBar/SearchBar/SearchBar'
+import RecentPosts from './Components/SideBar/RecentPosts/RecentPosts'
+import RecentComments from './Components/SideBar/RecentComments/RecentComments'
+import CustomBreadcrumbs from '../CustomBreadcrumbs/CustomBreadcrumbs'
+import { articlesBlogPage } from '../../Services/Utils/Data/data'
 
 function BlogPage() {
+  const [articles] = useState(articlesBlogPage)
+  const [articlesPerPage] = useState(6)
+  const [currentPage, setCurrentPage] = useState(1)
+  const indexOfLastArticle = currentPage * articlesPerPage
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage
+  const currentArticles = articles.slice(
+    indexOfFirstArticle,
+    indexOfLastArticle
+  )
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
+
   return (
     <>
-      <CustomBreadcrumbs title={"blog"} />
+      <CustomBreadcrumbs title={'blog'} />
       <Grid
         container
-        sx={{ py: 6.25, px: "15px", maxWidth: "1200px", mx: "auto", my: 0 }}
+        sx={{ py: 6.25, px: '15px', maxWidth: '1200px', mx: 'auto', my: 0 }}
       >
         <Grid item md={9}>
-          <Articles></Articles>
+          <Grid container>
+            {currentArticles.map((article) => (
+              <ArticleCard
+                title={article.title}
+                image={article.image}
+                author={article.author}
+                releaseDate={article.releaseDate}
+                status={article.status}
+              />
+            ))}
+          </Grid>
+          <Pagination
+            productsPerPage={articlesPerPage}
+            totalProducts={articles.length}
+            paginate={paginate}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
         </Grid>
         <Grid item xs={12} md={3}>
           <SearchBar></SearchBar>
@@ -23,7 +54,7 @@ function BlogPage() {
         </Grid>
       </Grid>
     </>
-  );
+  )
 }
 
-export default BlogPage;
+export default BlogPage
