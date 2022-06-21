5e7608fdffe38595733f1e17fab5f6e5dfcea062
Siavash Khani <siavash.khani200@gmail.com>
2022-06-21 16:02:39 +0430

    feat(Wishlist): social section added | responsive fix

--- src/Pages/Main/Wishlist/Wishlist.tsx
+++ src/Pages/Main/Wishlist/Wishlist.tsx
@@ -1,26 +1,51 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Box, styled, Typography } from "@mui/material";
 import { CustomBreadcrumbs } from "../../../Components";
-
-const wrapperStyle = {
-    margin: '3rem auto',
-
-}
-
-const titleStyle = {
-  fontWeight: "bold",
-  color: "#222",
-};
+import EmailIcon from "@mui/icons-material/Email";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TelegramIcon from '@mui/icons-material/Telegram';
+import {
+  wrapperStyle,
+  wishlistTitle,
+  socialBox,
+  SocialFlex,
+} from "../../../Styles/Wishlist";
 
 const Wishlist = () => {
   return (
     <>
       <CustomBreadcrumbs title={"Wishlist"} />
 
-      <Box maxWidth="lg" sx={wrapperStyle}>
-        <Typography variant="h3" sx={titleStyle}>
+      <Box maxWidth="1270px" sx={wrapperStyle}>
+        <Typography variant="h3" sx={wishlistTitle}>
           Default Wishlist
         </Typography>
+
+
+
+        <hr />
+        <Box sx={socialBox}>
+          <SocialFlex>
+            <Typography sx={{ color: "#555" }}>Share on</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 3,
+                pl: { md: "4rem", lg: "4rem" },
+                color: "#333",
+              }}
+            >
+              <FacebookIcon sx={{cursor:"pointer"}}/>
+              <TwitterIcon sx={{cursor:"pointer"}}/>
+              <TelegramIcon sx={{cursor:"pointer"}}/>
+              <WhatsAppIcon sx={{cursor:"pointer"}}/>
+              <ContentCopyIcon sx={{cursor:"pointer"}}/>
+              <EmailIcon />
+            </Box>
+          </SocialFlex>
+        </Box>
       </Box>
     </>
   );
---
+++ src/Styles/Wishlist/index.tsx
@@ -0,0 +1,34 @@
+import { styled } from "@mui/material";
+
+export const wrapperStyle = {
+  margin: "3rem auto",
+  padding: "0 1rem",
+};
+
+export const wishlistTitle = {
+  fontWeight: "bold",
+  color: "#222",
+  mb: 8,
+};
+
+export const socialBox = {
+  margin: "2rem 0",
+  display: "flex",
+  justifyContent: {
+    xs: "center",
+    sm: "center",
+    md: "flex-start",
+    lg: "flex-end",
+    xl: "flex-start",
+  },
+  alignItems: "center",
+};
+
+export const SocialFlex = styled("div")(({ theme }) => ({
+  display: " flex",
+  [theme.breakpoints.down("md")]: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
