c3de8b7ab3b3b2e135b99e89da4eb4efc5ff48ee
tohid <tohidebrahimi17@gmail.com>
2022-06-23 21:47:14 +0430

    feat(products):add intersection observer

--- src/Components/Products/Components/ProductCards/ProductCards.tsx
+++ src/Components/Products/Components/ProductCards/ProductCards.tsx
@@ -4,9 +4,23 @@ import { productCardData } from "../../../../Services/Utils/Data/data";
 
 const ProductCards = () => {
   return (
-    <Grid container spacing={3} marginTop="3rem" marginBottom="3rem">
+    <Grid
+      container
+      spacing={3}
+      marginTop="3rem"
+      marginBottom="3rem"
+      sx={{
+        overflow: "hidden",
+      }}
+    >
       {productCardData.map((item) => (
-        <ProductCard title={item.title} name={item.name} image={item.image} />
+        <ProductCard
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          name={item.name}
+          image={item.image}
+        />
       ))}
     </Grid>
   );
