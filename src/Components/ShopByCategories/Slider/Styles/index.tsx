d6c6c6c126ffe5ac1e2593d756bafee6d9c1b765
tohid <tohidebrahimi17@gmail.com>
2022-06-23 15:59:14 +0430

    fix(swiperArrow):fixed arrow responsive

--- src/Components/ShopByCategories/Slider/Styles/index.tsx
+++ src/Components/ShopByCategories/Slider/Styles/index.tsx
@@ -1,74 +1,93 @@
-import {styled} from "@mui/material/styles";
-import {Box} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
 import navigationIcon from "../../../../Assets/Icons/right-arrow.svg";
 
-export const Container = styled(Box)(({theme}) => ({
-    position: 'relative',
-    width: '100%',
-    [theme.breakpoints.up("xs")]: {
-        maxWidth: "90vw",
+export const Container = styled(Box)(({ theme }) => ({
+  position: "relative",
+  width: "100%",
+  [theme.breakpoints.up("xs")]: {
+    maxWidth: "90vw",
+  },
+  [theme.breakpoints.up("md")]: {
+    maxWidth: "85vw",
+  },
+  [theme.breakpoints.up("lg")]: {
+    maxWidth: "1080px",
+  },
+  ".swiper": {
+    paddingBottom: "73px",
+    width: "100%",
+  },
+  ".swiper-pagination-bullet": {
+    background: "white",
+    opacity: 1,
+    margin: "10px!important",
+  },
+  ".swiper-horizontal>.swiper-pagination-bullets": {
+    bottom: "0",
+  },
+  ".swiper-pagination-bullet-active": {
+    position: "relative",
+    "&::before": {
+      animation: "spin-dot 2s linear infinite",
+      top: "-110%",
+      left: "-110%",
+      borderRadius: "50%",
+      position: "absolute",
+      content: '""',
+      width: "22px",
+      height: "22px",
+      borderRight: " 2px solid white",
+      borderLeft: "2px solid transparent",
+      borderTop: "2px solid white",
+      borderBottom: "2px solid white",
+    },
+  },
+  ".swiper-button-next,.swiper-button-prev": {
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+    border: "1px solid rgba(255, 255, 255, 0.3)",
+    width: "43px",
+    height: "43px",
+    "&::after": {
+      backgroundImage: `url(${navigationIcon})`,
+      backgroundSize: "contain",
+      opacity: "0.3",
+      width: "24px",
+      height: "24px",
+      content: '""',
+    },
+    "&:hover": {
+      border: "1px solid rgba(255, 255, 255, 1)",
+      "&:after": {
+        opacity: "1",
+      },
+    },
+  },
+  ".swiper-button-next": {
+    [theme.breakpoints.up("md")]: {
+      right: "-55px",
     },
     [theme.breakpoints.up("lg")]: {
-        maxWidth: "1150px",
+      right: "-48px",
     },
-    ".swiper": {
-        paddingBottom: '73px',
-        width: '100%',
+    [theme.breakpoints.up("xl")]: {
+      right: "-75px",
     },
-    ".swiper-pagination-bullet": {
-        background: 'white',
-        opacity: 1,
-        margin: '10px!important'
+    top: "40%",
+  },
+  ".swiper-button-prev": {
+    [theme.breakpoints.up("md")]: {
+      left: "-55px",
     },
-    ".swiper-horizontal>.swiper-pagination-bullets": {
-        bottom: '0'
+    [theme.breakpoints.up("lg")]: {
+      left: "-48px",
     },
-    ".swiper-pagination-bullet-active": {
-        position: 'relative',
-        '&::before': {
-            animation: 'spin-dot 2s linear infinite',
-            top: '-110%',
-            left: '-110%',
-            borderRadius: '50%',
-            position: 'absolute',
-            content: '""',
-            width: '22px',
-            height: '22px',
-            borderRight: ' 2px solid white',
-            borderLeft: '2px solid transparent',
-            borderTop: '2px solid white',
-            borderBottom: '2px solid white',
-        }
+    [theme.breakpoints.up("xl")]: {
+      left: "-75px",
     },
-    ".swiper-button-next,.swiper-button-prev": {
-        [theme.breakpoints.down("lg")]: {
-            display: 'none'
-        },
-        border: '1px solid rgba(255, 255, 255, 0.3)',
-        width: '43px',
-        height: '43px',
-        '&::after': {
-            backgroundImage: `url(${navigationIcon})`,
-            backgroundSize: "contain",
-            opacity: "0.3",
-            width: '24px',
-            height: '24px',
-            content: '""'
-        },
-        "&:hover": {
-            border: '1px solid rgba(255, 255, 255, 1)',
-            '&:after': {
-                opacity: "1",
-            }
-        }
-    },
-    '.swiper-button-next': {
-        right: '-75px',
-        top: '40%',
-    },
-    ".swiper-button-prev": {
-        left: '-75px',
-        top: '40%',
-        transform: ' scale(-1, 1)'
-    },
-}))
+    top: "40%",
+    transform: " scale(-1, 1)",
+  },
+}));
