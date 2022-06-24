389c09fa9f619b38072db058670a14525047d371
monire <aglida1370@gmail.com>
2022-06-24 23:38:37 +0430

    feat(cart):added cart column data

---
+++ src/Components/ShoppingCart/Tables/CartListTable/data.tsx
@@ -0,0 +1,49 @@
+export const columnsData = [
+    {
+        id: 1,
+        name: '',
+        width: '5%'
+    },
+    {
+        id: 2,
+        name: '',
+        width: '10%'
+    },
+    {
+        id: 3,
+        name: 'Product',
+        width: '30%'
+    },
+    {
+        id: 4,
+        name: 'Price',
+        width: '15%'
+    },
+    {
+        id: 5,
+        name: 'Quantity',
+        width: '30%'
+    },
+    {
+        id: 6,
+        name: 'SUBTOTAL',
+        width: '10%'
+    }
+]
+
+export const CartListTableInput = {
+    margin: '0 8px 0 0',
+    width: "auto",
+    borderColor: '#ddd',
+    "& .MuiInputBase-input": {
+        fontSize: '14px',
+        padding: '0 0.8em',
+        height: "32px",
+        color: '#333',
+        backgroundColor: '#eaeaea',
+        "&::-webkit-inner-spin-button": {appearance: "none"},
+    },
+    ".MuiOutlinedInput-notchedOutline": {
+        border: 'none',
+    },
+}
