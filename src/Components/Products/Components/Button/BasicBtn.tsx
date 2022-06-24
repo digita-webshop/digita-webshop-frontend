fdc57e1adad0c2f6cacadf3a19e5074ddfe89d11
tohid <tohidebrahimi17@gmail.com>
2022-06-24 11:21:33 +0430

    feat(products):add product shop btn animation

--- src/Components/Products/Components/Button/BasicBtn.tsx
+++ src/Components/Products/Components/Button/BasicBtn.tsx
@@ -1,8 +1,32 @@
 import { Box, Button } from "@mui/material";
+import { useInView } from "react-intersection-observer";
 
 const BasicBtn = () => {
+  const { ref, inView } = useInView({ triggerOnce: true });
+
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", margin: "2rem 0" }}>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        margin: "2rem 0",
+        animation: (theme) =>
+          inView
+            ? `productBtnFadeIn 1500ms ${theme.transitions.easing.easeInOut}`
+            : "",
+        "@keyframes productBtnFadeIn": {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(200%)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        },
+      }}
+      ref={ref}
+    >
       <Button
         variant="contained"
         sx={{
