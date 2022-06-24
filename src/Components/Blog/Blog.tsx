e51e25129c0920cf36b3b50d0e144fa00431ee45
tohid <tohidebrahimi17@gmail.com>
2022-06-24 12:57:15 +0430

    fix(Blog):modify background & container & naming

--- src/Components/Blog/Blog.tsx
+++ src/Components/Blog/Blog.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import Pagination from '../Pagination/Pagination';
-import { Grid, useMediaQuery } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import ArticleCard from './Components/Articles/Article/ArticleCard';
-import SearchBar from './Components/SideBar/SearchBar/SearchBar';
-import RecentPosts from './Components/SideBar/RecentPosts/RecentPosts';
-import RecentComments from './Components/SideBar/RecentComments/RecentComments';
-import CustomBreadcrumbs from '../CustomBreadcrumbs/CustomBreadcrumbs';
-import { articlesBlogPage } from '../../Services/Utils/Data/data';
+import { useState } from "react";
+import Pagination from "../Pagination/Pagination";
+import { Box, Grid, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import ArticleCard from "./Components/Articles/Article/ArticleCard";
+import SearchBar from "./Components/SideBar/SearchBar/SearchBar";
+import RecentPosts from "./Components/SideBar/RecentPosts/RecentPosts";
+import RecentComments from "./Components/SideBar/RecentComments/RecentComments";
+import CustomBreadcrumbs from "../CustomBreadcrumbs/CustomBreadcrumbs";
+import { articlesBlogPage } from "../../Services/Utils/Data/data";
 
-function BlogPage() {
+function Blog() {
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('md'));
+  const matches = useMediaQuery(theme.breakpoints.up("md"));
   const [articles] = useState(articlesBlogPage);
   const [articlesPerPage] = useState(6);
   const [currentPage, setCurrentPage] = useState(1);
@@ -24,11 +24,11 @@ function BlogPage() {
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
-    <>
-      <CustomBreadcrumbs title={'blog'} />
-      <Grid container sx={{ py: 6.25, maxWidth: '1200px', mx: 'auto', my: 0 }}>
+    <Box sx={{ backgroundColor: "white", padding: 0, margin: 0 }}>
+      <CustomBreadcrumbs title={"blog"} />
+      <Grid container sx={{ py: 6.25, maxWidth: "1200px", mx: "auto", my: 0 }}>
         <Grid item md={9}>
-          <Grid sx={{ ml: '15px' }}>
+          <Grid sx={{ ml: "15px" }}>
             {!matches ? <SearchBar></SearchBar> : null}
           </Grid>
           <Grid container>
@@ -53,7 +53,7 @@ function BlogPage() {
           ) : null}
         </Grid>
         <Grid item xs={12} md={3}>
-          <Grid sx={{ ml: '15px' }}>
+          <Grid sx={{ ml: "15px" }}>
             {matches ? <SearchBar></SearchBar> : null}
             <RecentPosts></RecentPosts>
             <RecentComments></RecentComments>
@@ -69,8 +69,8 @@ function BlogPage() {
           ) : null}
         </Grid>
       </Grid>
-    </>
+    </Box>
   );
 }
 
-export default BlogPage;
+export default Blog;
