2a79f19d6146024f08283a40b6d0183513449dad
tohid <tohidebrahimi17@gmail.com>
2022-06-24 17:33:41 +0430

    style(pages):add bgcolor to all pages

--- src/Pages/Main/ContactUs/ContactUs.tsx
+++ src/Pages/Main/ContactUs/ContactUs.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { Container } from "@mui/system";
 import {
   ContactUsAddress,
@@ -9,25 +9,20 @@ import {
 
 function ContactUs() {
   return (
-    <>
+    <Box bgcolor={"white"}>
       <CustomBreadcrumbs title={"contact us "} />
       <Container maxWidth={"lg"}>
         <ContactUsAddress />
-        <Grid container spacing={3} marginY={6}>
+        <Grid container spacing={3} paddingY={6}>
           <Grid item xs={12} md={6}>
-            <Map
-            // googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
-            // loadingElement={<div style={{ height: `100%` }} />}
-            // containerElement={<div style={{ height: `400px` }} />}
-            // mapElement={<div style={{ height: `100%` }} />}
-            />
+            <Map />
           </Grid>
           <Grid item xs={12} md={6}>
             <ContactUsForm />
           </Grid>
         </Grid>
       </Container>
-    </>
+    </Box>
   );
 }
 
