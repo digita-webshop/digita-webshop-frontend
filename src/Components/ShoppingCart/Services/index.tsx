24124cde86e1153bfa63071579f07ce5d7fabfdd
monire <aglida1370@gmail.com>
2022-06-25 18:22:55 +0430

    fix(cart):remove based on id

--- src/Components/ShoppingCart/Services/index.tsx
+++ src/Components/ShoppingCart/Services/index.tsx
@@ -1,8 +1,8 @@
 import CartItem from "../Types/CartItemType";
 import React, {Dispatch} from "react";
 
-export const handleRowDelete = (index: number,setCartList:Dispatch<React.SetStateAction<CartItem[]>>) => {
+export const handleRowDelete = (index: number, setCartList: Dispatch<React.SetStateAction<CartItem[]>>) => {
     setCartList(prevState => {
-        return prevState.filter((item: CartItem, i: number) => i !== index)
+        return prevState.filter((item: CartItem) => item.id !== index)
     })
 }
