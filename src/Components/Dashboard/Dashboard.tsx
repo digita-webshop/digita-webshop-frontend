3d01e342a15bbb4e376b17ba2d37f13d6b8018f6
tohid <tohidebrahimi17@gmail.com>
2022-07-02 17:13:33 +0430

    feat(dashboard):add statistic card

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,11 +1,18 @@
-import { Grid } from "@mui/material";
+import { Grid, Icon, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import ContentHeader from "./ContentHeader/ContentHeader";
+import StatisticsCard from "./StatisticsCard/StatisticsCard";
 
 function Dashboard() {
   return (
-    <Grid container>
+    <Grid container spacing={4}>
       <Grid item xs={12}>
         <ContentHeader />
+      </Grid>
+      <Grid container item xs={12}>
+        <Grid xs={12} md={4}>
+          <StatisticsCard />
+        </Grid>
       </Grid>
     </Grid>
   );
