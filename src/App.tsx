4683e85c0b7744020fd139f4c98ebe47a2a965d9
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-07-03 15:31:19 +0200

    feat(blog article):add route to App & create BlogArticle component

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ import {
   Reviews,
   Settings,
 } from "./Pages/Panel";
-
+import BlogArticle from "./Components/Blog/BlogArticle/BlogArticle";
 function App() {
   return (
     <ThemeProvider theme={theme}>
@@ -31,23 +31,25 @@ function App() {
         <ScrollToTop>
           <Routes>
             <Route element={<MainLayout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/about-us" element={<AboutUs />} />
-              <Route path="/contact-us" element={<ContactUs />} />
-              <Route path="/wishlist" element={<Wishlist />} />
-              <Route path="/checkout" element={<Checkout />} />
+              <Route path='/' element={<Home />} />
+              <Route path='/shop' element={<Shop />} />
+              <Route path='/blog/:id' element={<BlogArticle />} />
+              <Route path='/blog/:id/*' element={<Blog />} />
+              <Route path='/blog' element={<Blog />} />
+              <Route path='/about-us' element={<AboutUs />} />
+              <Route path='/contact-us' element={<ContactUs />} />
+              <Route path='/wishlist' element={<Wishlist />} />
+              <Route path='/checkout' element={<Checkout />} />
             </Route>
-            <Route path="/panel/*" element={<PanelLayout />}>
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="products" element={<Products />} />
-              <Route path="orders" element={<Orders />} />
-              <Route path="add-product" element={<AddProduct />} />
-              <Route path="add-article" element={<AddArticle />} />
-              <Route path="reviews" element={<Reviews />} />
-              <Route path="brands" element={<Brands />} />
-              <Route path="settings" element={<Settings />} />
+            <Route path='/panel/*' element={<PanelLayout />}>
+              <Route path='dashboard' element={<Dashboard />} />
+              <Route path='products' element={<Products />} />
+              <Route path='orders' element={<Orders />} />
+              <Route path='add-product' element={<AddProduct />} />
+              <Route path='add-article' element={<AddArticle />} />
+              <Route path='reviews' element={<Reviews />} />
+              <Route path='brands' element={<Brands />} />
+              <Route path='settings' element={<Settings />} />
             </Route>
           </Routes>
         </ScrollToTop>
