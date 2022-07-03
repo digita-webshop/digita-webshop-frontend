d2401b9c8676de49ff438531e57fd861c118392f
tohid <tohidebrahimi17@gmail.com>
2022-07-03 16:19:09 +0430

    feat(orders):add table header cp

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -18,3 +18,4 @@ export { default as Shop } from "./Shop/Shop";
 export { default as Checkout } from "./Checkout/Checkout";
 export { default as Wishlist } from "./Wishlist/Wishlist";
 export { default as Dashboard } from "./Dashboard/Dashboard";
+export { default as Orders } from "./Orders/Orders";
