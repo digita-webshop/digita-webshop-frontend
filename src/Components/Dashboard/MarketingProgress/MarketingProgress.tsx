4e017e58e9a3610d9dbb4b19af3733604f5192b2
tohid <tohidebrahimi17@gmail.com>
2022-07-03 12:52:29 +0430

    feat(dashboard):marketing progress ui done

--- src/Components/Dashboard/MarketingProgress/MarketingProgress.tsx
+++ src/Components/Dashboard/MarketingProgress/MarketingProgress.tsx
@@ -1,18 +1,27 @@
-import { Box } from "@mui/material";
+import { Launch } from "@mui/icons-material";
+import { Box, Typography } from "@mui/material";
 import { marketingProgress } from "../../../Services/Utils/Data/data";
-import { DashTitle, DashWrapper } from "../../../Styles/Dashboard";
+import {
+  analyticsBtnStyles,
+  DashTitle,
+  DashWrapper,
+} from "../../../Styles/Dashboard";
 import ProgressCard from "./ProgressCard/ProgressCard";
 
 function MarketingProgress() {
   return (
     <DashWrapper>
       <DashTitle>marketing</DashTitle>
-      <Box my={4}>
+      <Box my={5}>
         {marketingProgress.map(({ id, title, percent, color }) => (
-          <Box key={id} sx={{ position: "relative" }}>
+          <Box key={id} sx={{ position: "relative", mb: "14px" }}>
             <ProgressCard title={title} percent={percent} color={color} />
           </Box>
         ))}
+      </Box>
+      <Box sx={analyticsBtnStyles}>
+        <Typography>Open analytics</Typography>
+        <Launch sx={{ color: "common.panelGrey" }} />
       </Box>
     </DashWrapper>
   );
