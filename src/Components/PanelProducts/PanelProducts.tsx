d6d5f52806a67573ee759a285a3fc8ac97aae142
Siavash Khani <siavash.khani200@gmail.com>
2022-07-05 23:34:36 +0430

    feat(ProductItem): pagination finished & product name bug fixed

--- src/Components/PanelProducts/PanelProducts.tsx
+++ src/Components/PanelProducts/PanelProducts.tsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import ContentHeader from "./ContentHeader/ContentHeader";
 import GridHeader from "./GridHeader/GridHeader";
-import { Grid, SelectChangeEvent, Divider } from "@mui/material";
-import { DashWrapper } from "../../Styles/PanelProducts";
+import { Grid, SelectChangeEvent, Divider, Box } from "@mui/material";
+import { DashWrapper, paginationStyle } from "../../Styles/PanelProducts";
 import Product from "./Product/Product";
 import { panelProducts } from "../../Services/Utils/Data/data";
+import Pagination from "../Pagination/Pagination";
 
 const PanelProducts = () => {
+  const [products] = useState(panelProducts);
   const [currentPage, setCurrentPage] = useState(1);
-  const [productPerPage] = useState(4);
-  const paginationNumbers = Math.ceil(FormData.length / productPerPage);
-  const indexOfLastContact = currentPage * productPerPage;
-  const indexOfFirstContact = indexOfLastContact - productPerPage;
+  const [productsPerPage] = useState(8);
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   const [selectedStatus, setSelectedStatus] = useState("status");
   const [selectedAmount, setSelectedAmount] = useState("20");
@@ -51,12 +57,21 @@ const PanelProducts = () => {
           }}
         >
           <Grid container spacing={2}>
-            {panelProducts.map(({id, name, price, image}) => (
+            {currentProducts.map(({ id, name, price, image }) => (
               <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={id}>
-                <Product id={id} name={name} price={price} image={image}/>
+                <Product id={id} name={name} price={price} image={image} />
               </Grid>
             ))}
           </Grid>
+          <Box sx={paginationStyle}>
+            <Pagination
+              productsPerPage={productsPerPage}
+              totalProducts={products.length}
+              paginate={paginate}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
+          </Box>
         </DashWrapper>
       </Grid>
     </Grid>
