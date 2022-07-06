35d24ff4e7f70f11f0c3a95131b139c67f16f2bf
Siavash Khani <siavash.khani200@gmail.com>
2022-07-06 22:02:54 +0430

    feat(Brand): working on each brand item

---
+++ src/Styles/Brands/index.tsx
@@ -0,0 +1,21 @@
+export const cardWrapper = {
+  width: "100%",
+  border: "1px solid #d8d8d8",
+  cursor: "pointer",
+};
+
+export const titleWrapper = {
+  textAlign: "center",
+  paddingBottom: "1rem !important",
+
+  webkitBoxOrient: "vertical",
+  display: "webkitBox",
+  overflow: "hidden !important",
+  webkitLineClamp: 1,
+  whiteSpace: "nowrap",
+};
+
+export const titleStyle = {
+  color: "#333",
+  fontSize: "19px",
+};
