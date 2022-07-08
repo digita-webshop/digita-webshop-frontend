9586278e62c5a2666f5ea199850e1d48e73bb4b9
tohid <tohidebrahimi17@gmail.com>
2022-07-08 19:13:56 +0430

    feat(settings):add general form

--- src/Components/Settings/General/General.tsx
+++ src/Components/Settings/General/General.tsx
@@ -1,10 +1,11 @@
-import { FormControl, FormLabel, Grid } from "@mui/material";
-import { PTextField } from "../../../Styles/panelCommon";
-import { formLabelStyles } from "../../../Styles/Settings";
+import { CloudUpload } from "@mui/icons-material";
+import { Box, FormControl, FormLabel, Grid } from "@mui/material";
+import { PButton, PTextField } from "../../../Styles/panelCommon";
+import { formLabelStyles, POutlinedButton } from "../../../Styles/Settings";
 
 function General() {
   return (
-    <Grid container>
+    <Grid container spacing={3}>
       <Grid container item xs={12} md={8} spacing={3}>
         <Grid item xs={12} sm={6}>
           <FormControl fullWidth>
@@ -18,10 +19,42 @@ function General() {
             <PTextField placeholder="Type Here" />
           </FormControl>
         </Grid>
-        <Grid item xs={12}></Grid>
-        <Grid item xs={12}></Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <FormLabel sx={formLabelStyles}>email</FormLabel>
+            <PTextField placeholder="example@email.com" />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <FormLabel sx={formLabelStyles}>phone</FormLabel>
+            <PTextField placeholder="+1234567890" />
+          </FormControl>
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={4}></Grid>
+      <Grid item xs={12} md={4}>
+        <Box>
+          <Box
+            sx={{
+              textAlign: "center",
+              img: { height: "195px", borderRadius: "50%" },
+            }}
+          >
+            <img
+              src="https://www.ecommerce-admin.com/demo/images/people/avatar1.jpg"
+              alt=""
+            />
+          </Box>
+          <Box sx={{ textAlign: "center", marginTop: "20px" }}>
+            <POutlinedButton variant="contained">
+              <CloudUpload sx={{ marginRight: "5px" }} /> upload
+            </POutlinedButton>
+          </Box>
+        </Box>
+      </Grid>
+      <Grid item xs={12}>
+        <PButton variant="contained">save changes</PButton>
+      </Grid>
     </Grid>
   );
 }
