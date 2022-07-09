1637a37130cfe4fe38cabdd6a6c44ebe5d6b8a5c
tohid <tohidebrahimi17@gmail.com>
2022-07-09 13:34:15 +0430

    feat(panelSettings):add password component

--- src/Components/Settings/Password/Password.tsx
+++ src/Components/Settings/Password/Password.tsx
@@ -1,5 +1,33 @@
+import { FormControl, FormLabel, Grid } from "@mui/material";
+import { PButton, PTextField } from "../../../Styles/panelCommon";
+import { formLabelStyles } from "../../../Styles/Settings";
+
 function Password() {
-  return <>password</>;
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={12}>
+        <FormControl fullWidth>
+          <FormLabel sx={formLabelStyles}>current password</FormLabel>
+          <PTextField placeholder="Type Here" />
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <FormControl fullWidth>
+          <FormLabel sx={formLabelStyles}>new password</FormLabel>
+          <PTextField placeholder="Type Here" />
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <FormControl fullWidth>
+          <FormLabel sx={formLabelStyles}>confirm password</FormLabel>
+          <PTextField placeholder="Type Here" />
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <PButton variant="contained">reset password</PButton>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Password;
