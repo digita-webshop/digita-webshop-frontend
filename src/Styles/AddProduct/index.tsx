6f001777cb3b970aa9fe19135e9782d2b24d0ec0
tohid <tohidebrahimi17@gmail.com>
2022-07-11 13:11:22 +0430

    feat(addProduct):add tag ui & logic

--- src/Styles/AddProduct/index.tsx
+++ src/Styles/AddProduct/index.tsx
@@ -11,3 +11,34 @@ export const ColorPickerWrapper = styled(Box)(({ theme }) => ({
   position: "relative",
   input: { position: "absolute", visibility: "hidden" },
 }));
+
+export const tagStyles = {
+  display: "flex",
+  alignItems: "center",
+  padding: "5px 10px",
+  cursor: "pointer",
+  border: "1px solid ",
+  borderColor: "common.panelHoverDarkGrey",
+  borderRadius: "4px",
+  boxSizing: "border-box",
+  color: "common.digitaBlack",
+  "&:hover": {
+    borderColor: "common.panelGrey",
+  },
+  span: {
+    fontSize: "14px",
+  },
+};
+
+export const tagContainerStyles = {
+  display: "flex",
+  flexWrap: "wrap",
+  marginTop: "20px",
+  gap: "10px",
+};
+
+export const tagIconStyles = {
+  color: "common.panelGrey",
+  fontSize: "18px",
+  marginLeft: "4px",
+};
