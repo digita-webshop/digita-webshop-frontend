41e92f98199438d977c73a1fc6612956b21313f7
tohid <tohidebrahimi17@gmail.com>
2022-07-12 12:26:33 +0430

    feat(addProduct):add gallery ui

--- src/Styles/AddProduct/index.tsx
+++ src/Styles/AddProduct/index.tsx
@@ -51,3 +51,63 @@ export const categoryCheckboxStyles = {
     fontSize: "20px",
   },
 };
+
+export const ImageWrapper = styled(Box)(({ theme }) => ({
+  position: "relative",
+  margin: "auto",
+  maxHeight: "168px",
+  maxWidth: "168px",
+  width: "100%",
+  height: "100%",
+  [theme.breakpoints.up("sm")]: {
+    width: "100%",
+    height: "60%",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  },
+
+  "&.main": {
+    height: "calc(100vmin - 67px)",
+    maxHeight: "unset",
+    maxWidth: "unset",
+    [theme.breakpoints.up("sm")]: {
+      height: "89%",
+    },
+    "& .MuiSvgIcon-root": {
+      top: "10px",
+      right: "6px",
+      padding: "12px",
+      fontSize: "30px",
+    },
+  },
+  input: {
+    visibility: "hidden",
+    position: "absolute",
+    width: "100%",
+  },
+  "& .MuiSvgIcon-root": {
+    boxShadow: "0px 0px 8px 0px rgb(0 0 0 / 15%)",
+    position: "absolute",
+    top: "10px",
+    right: "6px",
+    backgroundColor: "white",
+    borderRadius: "4px",
+    padding: "8px",
+    cursor: "pointer",
+    [theme.breakpoints.up("sm")]: {
+      top: "5px",
+      padding: "6px",
+    },
+    [theme.breakpoints.up("md")]: {
+      padding: "4px",
+      fontSize: "18px",
+    },
+    [theme.breakpoints.up("lg")]: {
+      padding: "8px",
+      fontSize: "22px",
+    },
+  },
+}));
