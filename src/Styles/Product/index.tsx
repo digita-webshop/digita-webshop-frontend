82df26afc87a05c36bc83609436b32f9458ee1a6
Samira-Abdi79 <samiraabdiurmia@gmail.com>
2022-07-12 08:37:19 -0700

    feat (Product) : add teamTitleStyle

---
+++ src/Styles/Product/index.tsx
@@ -0,0 +1,9 @@
+
+
+    /* ================   RELATED PRODUCTS ============== */
+    export const teamTitleStyle = {
+      padding: "8rem 0 1rem 0",
+      color: "#333",
+      fontSize: "32px",
+      fontWeight: "600",
+    };
