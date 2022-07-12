d3a5a85615a83ac3e2253b45f8c8ab00c45d7ba3
Samira-Abdi79 <samiraabdiurmia@gmail.com>
2022-07-12 08:46:50 -0700

    feat (Product) : add BoughtTogether component

--- src/Components/Product/Components/BoughtTogether/BoughtTogether.tsx
+++ src/Components/Product/Components/BoughtTogether/BoughtTogether.tsx
@@ -1,10 +1,27 @@
 
+
+  import {
+    Button,
+    Box,
+    Typography,
+  Grid,
+   Divider
+  } from "@mui/material";
 const BoughtTogether = () => {
   return( 
 
 
     < >
-
+    <Box sx={boughtPriceStyle}>
+  <Box sx={{margin:"-2rem 0 2rem 0"}}>
+   <Typography variant="body2" component="p" sx={BoughtTextStyle}>
+                 <ul className="boughtList">
+                         <li>This Product: Microsoft Xbox One S Blue Grey – <span className="newPrice">$52.00</span></li>
+                         <li>PC Gaming Mainboard B460 – <del className="previousPrice">$29.00</del> <span className="newPrice">$25.00</span></li>
+                 </ul>
+                 </Typography>
+   </Box>
+   </Box>
       </>  
   )
 };
