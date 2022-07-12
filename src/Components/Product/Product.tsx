411e2dc4964e7c0f9df948fb0293a7b6af5b3f9b
Samira-Abdi79 <samiraabdiurmia@gmail.com>
2022-07-12 08:30:44 -0700

    feat (Product) : add RelatedProducts component

--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -1,6 +1,16 @@
 
-import CustomBreadcrumbs from "../CustomBreadcrumbs/CustomBreadcrumbs";
+
+import {
+  
+  teamTitleStyle
+} from "../../Styles/Product/index";
+
+import { productData } from "../../Services/Utils/Data/data";
 import ProductDetails from "./Components/ProductDetails/ProductDetails"
+
+
+import CustomBreadcrumbs from "../CustomBreadcrumbs/CustomBreadcrumbs";
+import ProductItem from "./../Products/Components/ProductItem/ProductItem";
 
 import { Box, Container, Grid,Typography } from "@mui/material";
 
@@ -14,6 +24,31 @@ const Product = () => {
        <Container maxWidth={"lg"}>
        <ProductDetails />
        </Container>
+       <Container maxWidth={"lg"}>
+       <Box sx={{ textAlign: "center", pb: 8 }}>
+       <Typography variant="h4" sx={teamTitleStyle}>
+       RELATED PRODUCTS
+     </Typography>
+       <Grid container spacing={{ xs: 2, md: 2 }}>
+             {productData.slice(0, 4).map((item) => (
+              <Grid item xs={6} sm={4} md={3} key={item.id}>
+                <ProductItem
+                  id={item.id}
+                  name={item.name}
+                  image={item.image}
+                  offPrice={item.offPrice}
+                  price={item.price}
+                  sold={item.sold}
+                  starRate={item.starRate}
+                  description={item.description}
+                  listView={false}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+  
+      </Container>
 
 
     
