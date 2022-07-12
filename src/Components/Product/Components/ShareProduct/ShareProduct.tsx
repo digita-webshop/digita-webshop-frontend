2f721474dabd71fcfaea00f89c4797729f08d9a7
Samira-Abdi79 <samiraabdiurmia@gmail.com>
2022-07-12 09:19:58 -0700

    feat (Product) : add ShareProduct icons

--- src/Components/Product/Components/ShareProduct/ShareProduct.tsx
+++ src/Components/Product/Components/ShareProduct/ShareProduct.tsx
@@ -1,10 +1,40 @@
 
-import {Typography ,Box} from '@mui/material';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import MailIcon from '@mui/icons-material/Mail';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import { Grid, Box, Typography } from "@mui/material";
 
 const ShareProduct = () => {
    
     return(
         <Box >
+             
+<FacebookOutlinedIcon fontSize="large" className="Facebookback" sx={{
+                  color:"#000000",
+       fontSize:"45px" ,
+       float:"left",
+     
+                   }} />
+
+<TwitterIcon fontSize="large"    sx={{
+        fontSize:"25px" ,
+        
+        color: '#ffffff',
+      
+                  
+                   }}/>
+  <PinterestIcon fontSize="large" className="Pinteresback" sx={{
+                       color:"#000000",
+           fontSize:"45px" ,
+           float:"left",
+                      }} />
+    <MailIcon fontSize="large"  sx={{
+             fontSize:"25px" ,
+            
+             color: '#ffffff',
+           }}/>
+
 
       </Box>
     )
