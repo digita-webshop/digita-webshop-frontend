cfc49d9b195de197e86a50df8fd851b0eba272dd
Samira-Abdi79 <samiraabdiurmia@gmail.com>
2022-07-12 07:25:09 -0700

    feat (Pages) : add Product component

---
+++ src/Components/Product/Product.tsx
@@ -0,0 +1,21 @@
+
+import CustomBreadcrumbs from "../CustomBreadcrumbs/CustomBreadcrumbs";
+
+import { Box, Container, Grid,Typography } from "@mui/material";
+
+
+const Product = () => {
+  return( 
+
+
+    <Box bgcolor={"white"}>
+       <CustomBreadcrumbs title={"product"} />
+
+
+    
+      </Box>  
+  )
+};
+
+export default Product;
+ 
