816a0d9fddb8476f916e4a3b467837a11ef0060a
samira <samiraabdiurmia@gmail.com>
2022-07-13 01:32:52 -0700

    feat (Product) : update  ProductDetails grid and style

--- src/Components/Product/Components/ProductDetails/ProductDetails.tsx
+++ src/Components/Product/Components/ProductDetails/ProductDetails.tsx
@@ -12,30 +12,38 @@ import Stack from '@mui/material/Stack';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShuffleIcon from '@mui/icons-material/Shuffle';
 
-
+import {
+  ProductDetailsStyle,
+  productTitle,
+  contentWrap,
+  addToCartButton,
+  starRating
+  
+  
+  } from "../../../../Styles/Product/index";
 
 
 const ProductDetails = () => {
  return(
   <>
-  <Container maxWidth="sm" >
+  <Container maxWidth="lg"  sx={contentWrap}>
         
-   <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 2 }}>
-  <Grid item xs={6}>
+  <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
   
 
       
           
   </Grid>
-  <Grid item xs={6}>
+  <Grid item xs={12} md={6}>
   <Box sx={{ width: '100%', maxWidth: 500 }}>
       
-      <Typography variant="h4" gutterBottom component="div">
+      <Typography variant="h4" sx={productTitle} component="div">
       Microsoft Xbox One S Blue Grey
       </Typography>
       <Button variant="outlined"><ArrowBackIosIcon/></Button>
       <Button variant="outlined"><ArrowForwardIosIcon/></Button>
-      <Rating name="size-small" defaultValue={5} size="small" />
+      <Rating name="size-small" defaultValue={5} size="small" sx={starRating}/>
       <Link href="#" underline="hover">
       (1 customer review)
 </Link>
@@ -48,17 +56,17 @@ const ProductDetails = () => {
           }}
           variant="filled"
         />
-      <Typography variant="body2" gutterBottom>
+      <Typography variant="body2" sx={ProductDetailsStyle} component="div">
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
 Consetetur sadipscing elitr, sed diam nonumy eirmod
 Suspendisse ultrices mauris diam
       </Typography>
-      <Typography variant="body2" gutterBottom>
+      <Typography variant="body2" sx={ProductDetailsStyle} component="p" >
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat
       </Typography>
-      <Button variant="contained">Add To Cart</Button>
-      <Button variant="contained">View Cart</Button>
+      <Button variant="contained" sx={addToCartButton}>Add To Cart</Button>
+      {/* <Button variant="contained">View Cart</Button> */}
 
       <Stack direction="row" spacing={2}>
       <Button variant="outlined" startIcon={<FavoriteBorderIcon/>}>
@@ -74,10 +82,10 @@ Wishlist
     </Box>
     <Divider />
     <Box sx={{ width: '100%', maxWidth: 500 }}>
-    <Typography variant="caption" display="block" gutterBottom>
+    <Typography variant="body2" component="p" sx={{color: "#333", fontWeight: "600"}}>
     SKU: WVN-13
       </Typography>
-      <Typography variant="body2" gutterBottom>
+      <Typography variant="body2" component="p" sx={ProductDetailsStyle}>
       Categories: Audio &amp; Video Game, Headphone, Phone Accessories, Smart TV, Technologies, Virtual Reality Glasses, Xbox(Console)
 Tags: Tag-02, Tag-08
       </Typography>
