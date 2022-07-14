84d99f414ff67596ebc231971c55a89de71ace1b
samira <samiraabdiurmia@gmail.com>
2022-07-14 02:22:29 -0700

    feat (Product) : add AddToCartModal component

---
+++ src/Components/Product/Components/Modals/AddToCartModal/AddToCartModal.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+import {
+    wooPopupInner,
+    wooPopupContent
+    
+    
+    } from "../../../../../Styles/Product/index";
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BasicModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box >
+       <Box sx={wooPopupInner}>
+       <Box sx={wooPopupContent}>
+
+</Box>
+       </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
