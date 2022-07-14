0f121b4ab8acf37b760b061684b85b8a96a138b9
samira <samiraabdiurmia@gmail.com>
2022-07-14 14:12:41 -0700

    feat (Product) : add  Swiper in  ProductSlider

--- src/Components/Product/Components/ProductSlider/ProductSlider.tsx
+++ src/Components/Product/Components/ProductSlider/ProductSlider.tsx
@@ -1,12 +1,30 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Navigation, Pagination } from "swiper";
+import React from "react";
+import ReactDOM from "react-dom";
+import Slider from "./slider/slider";
+import "react-id-swiper/src/styles/scss/swiper.scss";
 
-import{Grid ,TextField,Button ,Divider, Box ,Link ,Rating ,Typography,Container, CardActionArea, CardActions}  from '@mui/material';
+import{Grid ,TextField,Button }  from '@mui/material';
 
 
 const ProductSlider = () => {
  return(
   <>
- 
-      </>
+  <div className="App">
+    <section className="section">
+      <div className="container header">
+        <h1 className="title">React Id Swiper dynamic slides w lazy load</h1>
+        <h3 className="subtitle">
+          Adds lazy load support to swiper implementation
+        </h3>
+      </div>
+      <div className="container">
+        <Slider />
+      </div>
+    </section>
+  </div>
+  </>
  )
 };
 
