5fb9580c7e6e529e540c2113e8e481c67d4fb925
samira <samiraabdiurmia@gmail.com>
2022-07-14 02:26:22 -0700

    feat (Product) : add ProductSlider component

---
+++ src/Components/Product/Components/ProductSlider/ProductSlider.tsx
@@ -0,0 +1,13 @@
+
+import{Grid ,TextField,Button ,Divider, Box ,Link ,Rating ,Typography,Container, CardActionArea, CardActions}  from '@mui/material';
+
+
+const ProductSlider = () => {
+ return(
+  <>
+ 
+      </>
+ )
+};
+
+export default ProductSlider;
