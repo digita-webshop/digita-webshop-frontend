c3071dd7629fdfcb2b20dd73eb36a0c68112ed0f
samira <samiraabdiurmia@gmail.com>
2022-07-15 18:09:14 -0700

    refacto (Product) : update AddToCartModal

--- src/Components/Product/Components/Modals/AddToCartModal/AddToCartModal.tsx
+++ src/Components/Product/Components/Modals/AddToCartModal/AddToCartModal.tsx
@@ -13,34 +13,23 @@ import {
     popupText,
     buttonsWrap,
     continueBtn,
-    cartBtn
+    cartBtn,
+    popupWrap
 
     
     
     } from "../../../../../Styles/Product/index";
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+
 
 export default function BasicModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+
 
   return (
-    <div>
+    <Box sx={popupWrap}>
     
    
-        <Box >
+        <Box sx={{width:"40%",height:"400px"}}>
        <Box sx={wooPopupInner}>
        <Box sx={wooPopupContent}>
        <Typography variant="h3" component="h3" sx={popupTitle}>
@@ -66,6 +55,6 @@ export default function BasicModal() {
        </Box>
         </Box>
 
-    </div>
+    </Box>
   );
 }
