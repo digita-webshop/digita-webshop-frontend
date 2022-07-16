14e23416766488369d20ea318eaf2a13ee81dca7
tohid <tohidebrahimi17@gmail.com>
2022-07-16 13:55:11 +0430

    fix(cart page):improve responsive design

--- src/Components/ShoppingCart/QuantityInput/index.tsx
+++ src/Components/ShoppingCart/QuantityInput/index.tsx
@@ -1,95 +1,119 @@
-import {Box, TextField} from "@mui/material";
-import {CustomBtn} from "../../../Styles/Cart";
-import React, {useEffect, useState} from "react";
+import { Box, TextField } from "@mui/material";
+import { CustomBtn } from "../../../Styles/Cart";
+import React from "react";
 import CartItem from "../Types/CartItemType";
 import store from "../../../redux/store";
 import actions from "../../../redux/actions";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
 type Props = {
-    updateButtonDisabled: boolean;
-    setUpdateButtonDisabled: React.Dispatch<React.SetStateAction<boolean>>;
-    row: CartItem;
-    cartList: any
-    values: CartItem[];
-    setValues: React.Dispatch<React.SetStateAction<CartItem[]>>
-}
+  updateButtonDisabled: boolean;
+  setUpdateButtonDisabled: React.Dispatch<React.SetStateAction<boolean>>;
+  row: CartItem;
+  cartList: any;
+  values: CartItem[];
+  setValues: React.Dispatch<React.SetStateAction<CartItem[]>>;
+};
 
-const QuantityInput = ({setUpdateButtonDisabled, row, values, setValues, updateButtonDisabled, cartList}: Props) => {
+const QuantityInput = ({
+  setUpdateButtonDisabled,
+  row,
+  values,
+  setValues,
+  updateButtonDisabled,
+  cartList,
+}: Props) => {
+  // @ts-ignore todo fix later
+  const quantities = useSelector((state) => state.cartReducer.quantities);
+  const quantity = quantities.filter((item: CartItem) => item.id === row.id)[0]
+    .quantity;
+  const handleDecValue = () => {
+    if (quantity === 0) {
+      return;
+    }
 
-    // @ts-ignore todo fix later
-    const quantities = useSelector(state => state.cartReducer.quantities);
-    const quantity = quantities.filter((item: CartItem) => item.id === row.id)[0].quantity
-    const handleDecValue = () => {
-        if (quantity === 0) {
-            return
-        }
+    const clone = [...quantities];
+    clone.forEach((element: CartItem, index: number) => {
+      if (element.id === row.id) {
+        clone[index] = { ...element, quantity: quantity - 1 };
+      }
+    });
 
-        const clone = [...quantities]
-        clone.forEach((element: CartItem, index: number) => {
-            if (element.id === row.id) {
-                clone[index] = {...element, quantity: quantity - 1};
-            }
-        });
+    // @ts-ignore todo fix me later
+    store.dispatch(actions.cart.setQuantity(clone));
+    updateButtonDisabled && setUpdateButtonDisabled(false);
+  };
 
-        // @ts-ignore todo fix me later
-        store.dispatch(actions.cart.setQuantity(clone));
-        updateButtonDisabled && setUpdateButtonDisabled(false)
-    };
+  const handleIncValue = () => {
+    const clone = [...quantities];
+    clone.forEach((element: CartItem, index: number) => {
+      if (element.id === row.id) {
+        clone[index] = { ...element, quantity: quantity + 1 };
+      }
+    });
 
-    const handleIncValue = () => {
-        const clone = [...quantities]
-        clone.forEach((element: CartItem, index: number) => {
-            if (element.id === row.id) {
-                clone[index] = {...element, quantity: quantity + 1};
-            }
-        });
+    // @ts-ignore todo fix me later
+    store.dispatch(actions.cart.setQuantity(clone));
+    updateButtonDisabled && setUpdateButtonDisabled(false);
+  };
 
-        // @ts-ignore todo fix me later
-        store.dispatch(actions.cart.setQuantity(clone));
-        updateButtonDisabled && setUpdateButtonDisabled(false)
-    };
-
-    return <Box
-        sx={{
-            p: "1rem 0",
-            display: "flex",
-            alignItems: "center",
-            gap: 1,
-        }}
+  return (
+    <Box
+      sx={{
+        p: "1rem 0",
+        display: "flex",
+        alignItems: "center",
+        gap: 1,
+      }}
     >
-        <Box sx={{display: "flex", alignItems: "center", height: "3rem", border: '1px solid #e4e4e4'}}>
-            <Box sx={CustomBtn} style={{borderRight: '1px solid #e4e4e4'}} onClick={handleDecValue}>
-                -
-            </Box>
-            <TextField
-                id="outlined-number"
-                type="number"
-                value={quantity}
-                sx={{
-                    width: "58px",
-                    height: "50px",
-                    "& .MuiInputBase-root": {
-                        height: "100%",
-                        borderRadius: "0",
-                    },
-                    "& .MuiInputBase-input": {
-                        textAlign: "center",
-                    },
-                    ".MuiOutlinedInput-notchedOutline": {
-                        border: 'none',
-                    },
-                    input: {
-                        "&::-webkit-inner-spin-button": {appearance: "none"},
-                    },
-                }}
-                size="small"
-            />
-            <Box sx={CustomBtn} onClick={handleIncValue} style={{borderLeft: '1px solid #e4e4e4'}}>
-                +
-            </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          height: { xs: "58px", sm: "40px", lg: "58px" },
+          border: "1px solid #e4e4e4",
+        }}
+      >
+        <Box
+          sx={CustomBtn}
+          style={{ borderRight: "1px solid #e4e4e4" }}
+          onClick={handleDecValue}
+        >
+          -
         </Box>
+        <TextField
+          id="outlined-number"
+          type="number"
+          value={quantity}
+          sx={{
+            width: { xs: "58px", sm: "40px", lg: "58px" },
+            height: { xs: "58px", sm: "40px", lg: "58px" },
+            "& .MuiInputBase-root": {
+              height: "100%",
+              borderRadius: "0",
+            },
+            "& .MuiInputBase-input": {
+              textAlign: "center",
+            },
+            ".MuiOutlinedInput-notchedOutline": {
+              border: "none",
+            },
+            input: {
+              "&::-webkit-inner-spin-button": { appearance: "none" },
+            },
+          }}
+          size="small"
+        />
+        <Box
+          sx={CustomBtn}
+          onClick={handleIncValue}
+          style={{ borderLeft: "1px solid #e4e4e4" }}
+        >
+          +
+        </Box>
+      </Box>
     </Box>
-}
+  );
+};
 
-export default QuantityInput
+export default QuantityInput;
--- src/Components/ShoppingCart/Tables/CartListTable/index.tsx
+++ src/Components/ShoppingCart/Tables/CartListTable/index.tsx
@@ -1,70 +1,103 @@
-import {Box, Paper, Table, TableBody, TableContainer, TableHead, TableRow} from "@mui/material";
-import {StyledTableCell} from "../../../../Styles/Cart";
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { StyledTableCell } from "../../../../Styles/Cart";
 import DynamicButton from "../../DynamicButton/DynamicButton";
-import React, {Dispatch, useState} from "react";
+import React, { Dispatch, useState } from "react";
 import CartItem from "../../Types/CartItemType";
-import {columnsData} from "./data";
+import { columnsData } from "./data";
 import CartListTableRow from "../CartListTableRow";
-import {UpdateCart} from "../../ShoppingCart";
-import {UpdateType} from "../../CartUpdated";
+import { UpdateCart } from "../../ShoppingCart";
+import { UpdateType } from "../../CartUpdated";
 import store from "../../../../redux/store";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import actions from "../../../../redux/actions";
 
 type Props = {
-    cartList: CartItem[];
-    values: CartItem[];
-    setValues: React.Dispatch<React.SetStateAction<CartItem[]>>
-    setCartUpdated: React.Dispatch<React.SetStateAction<UpdateCart | null>>
-}
+  cartList: CartItem[];
+  values: CartItem[];
+  setValues: React.Dispatch<React.SetStateAction<CartItem[]>>;
+  setCartUpdated: React.Dispatch<React.SetStateAction<UpdateCart | null>>;
+};
 
-const CartListTable = ({cartList, values, setValues, setCartUpdated}: Props) => {
-    const [updateButtonDisabled, setUpdateButtonDisabled] = useState(true);
+const CartListTable = ({
+  cartList,
+  values,
+  setValues,
+  setCartUpdated,
+}: Props) => {
+  const [updateButtonDisabled, setUpdateButtonDisabled] = useState(true);
+  // @ts-ignore todo fix later
+  const quantities = useSelector((state) => state.cartReducer.quantities);
+
+  const handleUpdateCart = () => {
+    setUpdateButtonDisabled(true);
+
     // @ts-ignore todo fix later
-    const quantities = useSelector(state => state.cartReducer.quantities);
+    store.dispatch(actions.cart.setCart(quantities));
 
-    const handleUpdateCart = () => {
-        setUpdateButtonDisabled(true)
+    setCartUpdated((prevState) => {
+      return { ...prevState, type: UpdateType.Update };
+    });
+  };
 
-        // @ts-ignore todo fix later
-        store.dispatch(actions.cart.setCart(quantities));
-
-        setCartUpdated(prevState => {
-            return {...prevState, type: UpdateType.Update}
-        })
-    }
-
-    return <TableContainer component={Paper} sx={{height: '100%',marginBottom:'20px'}}>
-        <Table
-            sx={{minWidth: 700, border: '1px solid #ebebeb'}} aria-label="customized table">
-            <TableHead>
-                <TableRow>
-                    {columnsData.map((column) => {
-                        return <StyledTableCell key={column.id} width={column.width}>{column.name}</StyledTableCell>
-                    })}
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {cartList.map((row,) => (
-                    <CartListTableRow key={row.id} row={row} cartList={cartList}
-                                      setUpdateButtonDisabled={setUpdateButtonDisabled}
-                                      updateButtonDisabled={updateButtonDisabled}
-                                      values={values} setValues={setValues}
-                                      setCartUpdated={setCartUpdated}
-                    />
-                ))}
-                <TableRow>
-                    <StyledTableCell colSpan={6} align="left">
-                        <Box sx={{maxWidth: '360px', margin: '0 auto'}}>
-                            <DynamicButton title='Update cart'
-                                           disabled={updateButtonDisabled}
-                                           action={updateButtonDisabled ? undefined : handleUpdateCart}/>
-                        </Box>
-                    </StyledTableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
+  return (
+    <TableContainer
+      component={Paper}
+      sx={{ height: "100%", marginBottom: "20px" }}
+    >
+      <Table sx={{ border: "1px solid #ebebeb" }} aria-label="customized table">
+        <TableHead sx={{ display: { xs: "none", sm: "table-header-group" } }}>
+          <TableRow>
+            {columnsData.map((column) => {
+              return (
+                <StyledTableCell key={column.id} width={column.width}>
+                  {column.name}
+                </StyledTableCell>
+              );
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cartList.map((row) => (
+            <CartListTableRow
+              key={row.id}
+              row={row}
+              cartList={cartList}
+              setUpdateButtonDisabled={setUpdateButtonDisabled}
+              updateButtonDisabled={updateButtonDisabled}
+              values={values}
+              setValues={setValues}
+              setCartUpdated={setCartUpdated}
+            />
+          ))}
+          <TableRow>
+            <StyledTableCell colSpan={6} align="left">
+              <Box
+                sx={{
+                  width: "100%",
+                  maxWidth: { md: "240px" },
+                  marginLeft: "auto",
+                }}
+              >
+                <DynamicButton
+                  title="Update cart"
+                  disabled={updateButtonDisabled}
+                  action={updateButtonDisabled ? undefined : handleUpdateCart}
+                />
+              </Box>
+            </StyledTableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </TableContainer>
-}
+  );
+};
 
-export default CartListTable
+export default CartListTable;
