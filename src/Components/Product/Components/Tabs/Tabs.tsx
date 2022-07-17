8a1092f1d48b467cfa1c752bf4e5a82c22b3ede4
samira <samiraabdiurmia@gmail.com>
2022-07-17 08:10:25 -0700

    feat (Product) : update tabs

--- src/Components/Product/Components/Tabs/Tabs.tsx
+++ src/Components/Product/Components/Tabs/Tabs.tsx
@@ -1,10 +1,17 @@
+import * as React from 'react';
+import { styled } from '@mui/system';
+import TabsUnstyled from '@mui/base/TabsUnstyled';
+import TabsListUnstyled from '@mui/base/TabsListUnstyled';
+import TabPanelUnstyled from '@mui/base/TabPanelUnstyled';
+import { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
+import TabUnstyled, { tabUnstyledClasses } from '@mui/base/TabUnstyled';
 
 import {
-  tabsStyle,
+  
   ShowStyle,
   CascadingTabsTitle,
   CascadingTabs,
-  TabLabel,
+ 
 
 
 } from "../../../../Styles/Product";
@@ -12,85 +19,88 @@ import Description from "./Description/Description";
 import Delivery from "./Delivery/Delivery";
 import AboutBranad from "./AboutBranad/AboutBranad";
 import Reviews from "./Reviews/Reviews";
-import * as React from 'react';
+
 import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+// import Tab from '@mui/material/Tab';
 
 import{ Box,Container,Typography} from '@mui/material';
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
 
 
-export default function CenteredTabs() {
-  const [value, setValue] = React.useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+const Tab = styled(TabUnstyled)`
+white-space: nowrap;
+height:100%;
+font-size:20px;
+font-weight: 500;
+text-transform: uppercase;
+padding: 19px 20px;
+line-height: 1;
+display: inline-block !important;
+background-color: #f5f5f5;
+border: none;
+color: #ababab;
+margin: 0 1rem;
+ 
 
+  &.${tabUnstyledClasses.selected} {
+   border-bottom-style:solid;
+   border-bottom-color:#F03637;
+   color:#333333;
+  }
+
+  &.${buttonUnstyledClasses.disabled} {
+
+  }
+`;
+
+const TabPanel = styled(TabPanelUnstyled)`
+margin-top:30px;
+  width: 100%;
+
+`;
+
+const TabsList = styled(TabsListUnstyled)`
+display: block;
+
+width: 100%;
+
+margin-bottom: 20px;
+text-align: center;
+padding: 0px 20px;
+margin-top: 0px;
+background-color: #f5f5f5;
+border: none;
+overflow-x: auto;
+    z-index: 1;
+box-sizing: border-box;
+
+
+
+
+border-color: #eaeaea;
+`;
+export default function UnstyledTabsCustomized() {
   return (
     <Box sx={ShowStyle}>
     <Box className="TabsShow" sx={{width:"100%"}}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-      <Tabs value={value} onChange={handleChange}
-       centered
-        
-        sx={tabsStyle}
-        
-        >
-          <Tab label="DESCRIPTION" {...a11yProps(0)} sx={TabLabel} />
-          <Tab label="REVIEWS (1)" {...a11yProps(1)} sx={TabLabel}/>
-          <Tab label="ABOUT BRANDS" {...a11yProps(2)}sx={TabLabel} />
-          <Tab label="SHIPPING 	&#38; DELIVERY" {...a11yProps(3)}sx={TabLabel} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-      <Description />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-      <Reviews />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-      <AboutBranad />
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-      <Delivery />
-      </TabPanel>
+    <TabsUnstyled defaultValue={0}>
+      <TabsList>
+        <Tab>DESCRIPTION</Tab>
+        <Tab>REVIEWS (1)</Tab>
+        <Tab>ABOUT BRANDS</Tab>
+         <Tab> SHIPPING 	&#38; DELIVERY</Tab>
+      </TabsList>
+      <TabPanel value={0}><Description /></TabPanel>
+      <TabPanel value={1}><Reviews /></TabPanel>
+      <TabPanel value={2}> <AboutBranad /></TabPanel>
+      <TabPanel value={3}> <Delivery /></TabPanel>
+    </TabsUnstyled>
+      
     </Box>
 
 
-    {/* ////// */}
+  
 <Box className="CascadinShow">
 {/* <Container maxWidth={"lg"}> */}
 <Box>
@@ -103,9 +113,9 @@ export default function CenteredTabs() {
               <Description />
        
 </Box>
-       {/* </Container> */}
-       {/* <Container maxWidth={"lg"}> */}
-  <Box>     <Box  sx={CascadingTabs}>
+      
+  <Box>    
+     <Box  sx={CascadingTabs}>
               
               <Typography variant="h4" sx={CascadingTabsTitle} component="div">
               REVIEWS (1)
@@ -114,9 +124,7 @@ export default function CenteredTabs() {
                      <Reviews />
                      </Box>
        
-       {/* </Container> */}
-      
-       {/* <Container maxWidth={"lg"}> */}
+  
    <Box>
    <Box  sx={CascadingTabs}>
               
@@ -126,7 +134,7 @@ export default function CenteredTabs() {
                      <AboutBranad />
    </Box>
        
-       {/* </Container> */}
+ 
       
    <Box>
    <Box  sx={CascadingTabs}>
