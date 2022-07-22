435c5df1407234f90f0794f3bc58e6faf95e75eb
Siavash Khani <siavash.khani200@gmail.com>
2022-07-22 21:15:48 +0430

    feat(EmptyList): seperated component to reusable && added pagination for user wishlist items

---
+++ src/Components/EmptyList/EmptyList.tsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from "@mui/material";
+import { EmptyWishlistWrapper } from "../../Styles/EmptyList";
+
+interface T {
+    title: string;
+    image: string;
+}
+
+const EmptyList = ({title, image}: T) => {
+  return (
+    <EmptyWishlistWrapper>
+      <Box
+        component="img"
+        src={image}
+        alt="empty"
+        className="itemImg"
+      />
+      <Typography sx={{ fontWeight: "400", fontSize: "17px" }}>
+        Your {title} is empty
+      </Typography>
+    </EmptyWishlistWrapper>
+  );
+};
+
+export default EmptyList;
---
+++ src/Styles/EmptyList/index.tsx
@@ -0,0 +1,9 @@
+import { Box, styled } from "@mui/material";
+
+export const EmptyWishlistWrapper = styled(Box)(({ theme }) => ({
+  height: "60vh",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+}));
