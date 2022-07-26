71ebdddb83192d186373d6cf4bb1e207989f51f9
tohid <tohidebrahimi17@gmail.com>
2022-07-26 21:25:35 +0430

    refactor(product):remove extra styles

--- src/Components/Product/Components/BoughtTogether/BoughtTogether.tsx
+++ src/Components/Product/Components/BoughtTogether/BoughtTogether.tsx
@@ -1,18 +1,16 @@
-import productOne from "../../../../Assets/Images/Product/product-7.jpg";
-import productTwo from "../../../../Assets/Images/Product/product-13.jpg";
 import {
-  allBought,
   boughtTogether,
   boughttitleStyle,
   boughtPriceStyle,
-  imageBought,
   BoughtTextStyle,
   BoughtButton,
 } from "../../../../Styles/Product";
 
-import { Button, Box, Typography, Grid, Divider } from "@mui/material";
+import { Button, Box, Typography, Divider } from "@mui/material";
 
 import { Card, CardMedia, CardActionArea } from "@mui/material";
+import { productData } from "../../../../Services/Utils/Data/data";
+import { Fragment } from "react";
 
 const BoughtTogether = () => {
   return (
@@ -22,53 +20,50 @@ const BoughtTogether = () => {
       </Typography>
       <Divider sx={{ fontSize: "100px", fontWeight: "900" }} />
 
-      <Box sx={boughtTogether}>
-        {/* ===========================imageBought================ */}
-        <Box sx={imageBought}>
-          <Card sx={{ border: " 1px solid #ebebeb", margin: "0 0.5rem " }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="96"
-                width="96"
-                image={productOne}
-                alt="green iguana"
-              />
-            </CardActionArea>
-          </Card>
-
-          <Typography variant="body2" component="p" sx={BoughtTextStyle}>
-            +
-          </Typography>
-          <Card sx={{ border: " 1px solid #ebebeb", margin: "0 0.5rem" }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="96"
-                width="96"
-                image={productTwo}
-                alt="green iguana"
-              />
-            </CardActionArea>
-          </Card>
+      <Box sx={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+        <Box sx={boughtTogether}>
+          {/* ===========================imageBought================ */}
+          {productData.slice(1, 3).map((product, index) => (
+            <Fragment>
+              {index !== 0 && (
+                <Typography
+                  variant="body2"
+                  component="span"
+                  sx={BoughtTextStyle}
+                >
+                  +
+                </Typography>
+              )}
+              <Box>
+                <Card
+                  sx={{ border: " 1px solid #ebebeb", margin: "0 0.5rem " }}
+                >
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="96"
+                      width="96"
+                      image={product.image}
+                      alt="green iguana"
+                    />
+                  </CardActionArea>
+                </Card>
+              </Box>
+            </Fragment>
+          ))}
         </Box>
-
         {/* ===========================allBought================ */}
-        <Box sx={allBought}>
-          <Typography variant="body2" component="p" sx={BoughtTextStyle}>
-            <p>
-              {" "}
-              Price for all: <span className="newPrice">$77.00</span>
-            </p>
-
-            <Button sx={BoughtButton} variant="contained" color="error">
-              Add All To Cart
-            </Button>
+        <Box>
+          <Typography sx={{ marginTop: "20px", fontSize: "14px" }}>
+            Price for all: <span className="newPrice">$77.00</span>
           </Typography>
+          <Button sx={BoughtButton} variant="contained" color="error">
+            Add All To Cart
+          </Button>
         </Box>
       </Box>
 
-      <Box sx={{ margin: "-2rem 0 2rem 0" }}>
+      <Box>
         <Typography variant="body2" component="p" sx={BoughtTextStyle}>
           <ul className="boughtList">
             <li>
