1618d9188944351dba9ab488c14954a8476134ee
tohid <tohidebrahimi17@gmail.com>
2022-07-28 11:30:05 +0430

    feat(product):add share section ui

--- src/Services/Utils/Data/data.ts
+++ src/Services/Utils/Data/data.ts
@@ -1726,6 +1726,26 @@ export const orders = [
   },
 ];
 
+export const productShareItems=[
+  {
+    id:'1',
+    name:'facebook',
+    color:'#4267b2'
+  },{
+    id:'2',
+    name:'twitter',
+    color:'#0693e3'
+  },{
+    id:'3',
+    name:'mail',
+    color:'#61CE70'
+  },{
+    id:'4',
+    name:'pin',
+    color:'#E60023'
+  },
+]
+
 // const productModel=[{
 //   id: 1,
 //   title: "Beats by Dr. Dre Studio (Orange)",
