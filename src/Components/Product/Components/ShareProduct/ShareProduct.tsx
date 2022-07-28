0ff72d30a5460444cd4537f8efe565edd9c66eee
tohid <tohidebrahimi17@gmail.com>
2022-07-28 11:30:05 +0430

    feat(product):add share section ui

--- src/Components/Product/Components/ShareProduct/ShareProduct.tsx
+++ src/Components/Product/Components/ShareProduct/ShareProduct.tsx
@@ -1,130 +1,85 @@
-import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-import MailIcon from "@mui/icons-material/Mail";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import PinterestIcon from "@mui/icons-material/Pinterest";
 import { Grid, Box, Typography } from "@mui/material";
+import { productShareItems } from "../../../../Services/Utils/Data/data";
 import {
-  shareProductText,
-  MailIconLayout,
-  TwitterIconLayout,
-  chanceTwitter,
-  ShareStyle,
-  itemStyle,
-  chanceMail,
-  chanceFacebook,
-  chancePinteres,
-} from "../../../../Styles/Product/index";
+  PinterestIcon,
+  FacebookIcon,
+  MailIcon,
+  TwitterIcon,
+} from "../../../../Assets/Icons/product";
 
 const ShareProduct = () => {
   return (
-    <Grid container sx={ShareStyle}>
-      {/* ======== FacebookOutlinedIcon============================ ======= */}
-      <Grid item xs={6} sm={6} md={3} lg={3}>
-        <Box sx={chanceFacebook}>
-          <Box sx={itemStyle}>
-            <FacebookOutlinedIcon
-              fontSize="large"
-              className="Facebookback"
+    <Grid
+      container
+      sx={{
+        margin: "4rem 0",
+        "& svg": {
+          m: 1.5,
+        },
+      }}
+    >
+      {productShareItems.map(({ id, name, color }, index) => (
+        <Grid item xs={6} sm={6} md={3} key={id}>
+          <Box
+            sx={{
+              padding: "20px 0",
+              border: "1px solid #e9e9e9",
+            }}
+          >
+            <Box
               sx={{
-                color: "#000000",
-                fontSize: "45px",
-                float: "left",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "fit-content",
+                margin: "auto",
+                cursor: "pointer",
+                gap: "10px",
+                flexDirection: { xs: "column", md: "row" },
+                "&:hover": {
+                  "& .icon": {
+                    backgroundColor: color,
+                  },
+                  "& .text": {
+                    color: color,
+                  },
+                },
               }}
-            />
-            <Box sx={{ marginLeft: "0" }}>
-              <Typography
-                variant="body2"
-                className="Facebooktext"
-                component="p"
-                sx={shareProductText}
-              >
-                Share on Facebook
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-      </Grid>
-
-      {/* ======== TwitterIcon===================== ======= */}
-      <Grid item xs={6} sm={6} md={3} lg={3}>
-        <Box sx={chanceTwitter}>
-          <Box sx={itemStyle}>
-            <Box sx={TwitterIconLayout} className="Twitterback">
-              <TwitterIcon
-                fontSize="large"
+            >
+              <Box
                 sx={{
-                  fontSize: "25px",
-
-                  color: "#ffffff",
+                  display: "flex",
+                  backgroundColor: "common.digitaBlack",
+                  transition: "all 150ms",
+                  borderRadius: "50%",
+                  "& .MuiSvgIcon-root": { color: "white" },
                 }}
-              />
-            </Box>
-            <Box sx={{ marginLeft: "0.6rem" }}>
-              <Typography
-                variant="body2"
-                component="p"
-                sx={shareProductText}
-                className="Twittertext"
+                className="icon"
               >
-                Tweet This Product
-              </Typography>
+                {name === "twitter" && <TwitterIcon />}
+                {name === "facebook" && <FacebookIcon />}
+                {name === "mail" && <MailIcon />}
+                {name === "pin" && <PinterestIcon />}
+              </Box>
+              <Box>
+                <Typography
+                  component="p"
+                  sx={{
+                    color: "common.digitaBlack",
+                    transition: "all 150ms",
+                    fontSize: { xs: "14px", sm: "13px" },
+                  }}
+                  className="text"
+                >
+                  {name === "facebook"
+                    ? "Share on Facebook"
+                    : `${name} this product`}
+                </Typography>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </Grid>
-      {/* ======== PinterestIcon============================ ======= */}
-      <Grid item xs={6} sm={6} md={3} lg={3}>
-        <Box sx={chancePinteres}>
-          <Box sx={itemStyle}>
-            <PinterestIcon
-              fontSize="large"
-              className="Pinteresback"
-              sx={{
-                color: "#000000",
-                fontSize: "45px",
-                float: "left",
-              }}
-            />
-            <Box sx={{ marginLeft: "0" }}>
-              <Typography
-                variant="body2"
-                component="p"
-                className="Pinterestext"
-                sx={shareProductText}
-              >
-                Pin This Product
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-      </Grid>
-      {/* ==========================MailIcon================================ */}
-      <Grid item xs={6} sm={6} md={3} lg={3}>
-        <Box sx={chanceMail}>
-          <Box sx={itemStyle}>
-            <Box sx={MailIconLayout} className="mailback">
-              <MailIcon
-                fontSize="large"
-                sx={{
-                  fontSize: "25px",
-
-                  color: "#ffffff",
-                }}
-              />
-            </Box>
-            <Box sx={{ marginLeft: "0.6rem" }}>
-              <Typography
-                variant="body2"
-                component="p"
-                sx={shareProductText}
-                className="mailtext"
-              >
-                Mail This Product
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-      </Grid>
+        </Grid>
+      ))}
     </Grid>
   );
 };
