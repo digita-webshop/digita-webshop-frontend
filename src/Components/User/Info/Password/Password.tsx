4dfa4016f33b93ed45d24d9363f2f27d81a1d03b
Siavash Khani <siavash.khani200@gmail.com>
2022-07-30 20:45:39 +0430

    feat(Info): updating general and password components

--- src/Components/User/Info/Password/Password.tsx
+++ src/Components/User/Info/Password/Password.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { FormControl, Grid } from "@mui/material";
+import { PButton, PFormLabel, PTextField } from "../../../../Styles/panelCommon";
 
 const Password = () => {
   return (
-    <div>Password</div>
-  )
-}
+    <Grid container spacing={4}>
+      <Grid item xs={12}>
+        <FormControl fullWidth>
+          <PFormLabel>current password</PFormLabel>
+          <PTextField placeholder="Type Here" />
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <FormControl fullWidth>
+          <PFormLabel>new password</PFormLabel>
+          <PTextField placeholder="Type Here" />
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <FormControl fullWidth>
+          <PFormLabel>confirm password</PFormLabel>
+          <PTextField placeholder="Type Here" />
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <PButton variant="contained">reset password</PButton>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default Password
+export default Password;
