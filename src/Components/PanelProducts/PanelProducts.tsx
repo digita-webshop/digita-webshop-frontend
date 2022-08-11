f0b02f4083b12410d3697519ba820d11f71282bc
Siavash Khani <siavash.khani200@gmail.com>
2022-08-11 22:55:04 +0430

    refactore(articles): header and body of component is now reusable

--- src/Components/PanelProducts/PanelProducts.tsx
+++ src/Components/PanelProducts/PanelProducts.tsx
@@ -34,7 +34,7 @@ const PanelProducts = () => {
   return (
     <Grid container rowSpacing={4}>
       <Grid item xs={12}>
-        <ContentHeader />
+        <ContentHeader title="Products"/>
       </Grid>
 
       <Grid item xs={12}>
