5689064976a9dbbaaaff936618e6b072b770b9c9
tohid <tohidebrahimi17@gmail.com>
2022-08-18 16:23:15 +0430

    feat(ourBlog):add article links & title styles

--- src/Components/OurBlog/BlogCard/BlogCard.tsx
+++ src/Components/OurBlog/BlogCard/BlogCard.tsx
@@ -1,42 +1,60 @@
-import {Box, Typography} from "@mui/material";
-import {styled} from "@mui/material/styles";
-import Grid from '@mui/material/Grid';
-import PermIdentityIcon from '@mui/icons-material/PermIdentity';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import {Card, iconStyle, textStyle} from "./Styles";
+import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import { Card, iconStyle, textStyle } from "./Styles";
+import { Link } from "react-router-dom";
 
-function BlogCard({item}: any) {
-
-    return (
-        <Card>
-            <Box>
-                <img src={item.image} alt='profile-image'/>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <Box sx={{display: 'flex'}}>
-                            <Box sx={{display: 'flex', alignItems: 'center', marginRight: '12px'}}>
-                                <PermIdentityIcon sx={iconStyle}/>
-                                <Typography sx={textStyle}>{item.username}</Typography>
-                            </Box>
-                            <Box sx={{display: 'flex', alignItems: 'center'}}>
-                                <AccessTimeIcon sx={iconStyle}/>
-                                <Typography sx={textStyle}>{item.time}</Typography>
-                            </Box>
-                        </Box>
-
-                    </Grid>
-                    <Grid item xs={12} sx={{paddingTop: '0!important'}}>
-                        <Typography variant="h2" sx={{
-                            lineHeight: '1.3',
-                            fontSize: '20px',
-                            fontWeight: 500,
-                            color: '#333333'
-                        }}>{item.title}</Typography>
-                    </Grid>
-                </Grid>
+function BlogCard({ item }: any) {
+  return (
+    <Card>
+      <Box>
+        <Link to={`article/${item.id}`}>
+          <img src={item.image} alt="article" />
+        </Link>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Box sx={{ display: "flex" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginRight: "12px",
+                }}
+              >
+                <PermIdentityIcon sx={iconStyle} />
+                <Typography sx={textStyle}>{item.author}</Typography>
+              </Box>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <AccessTimeIcon sx={iconStyle} />
+                <Typography sx={textStyle}>{item.releaseDate}</Typography>
+              </Box>
             </Box>
-        </Card>
-    );
+          </Grid>
+          <Grid item xs={12} sx={{ paddingTop: "0!important" }}>
+            <Typography
+              variant="h2"
+              sx={{
+                lineHeight: "1.3",
+                fontSize: "20px",
+                fontWeight: 500,
+                a: {
+                  color: "#333333",
+                  textDecoration: "none",
+                  transition: "all 200ms",
+                  "&:hover": {
+                    color: "common.digitaRed",
+                  },
+                },
+              }}
+            >
+              <Link to={`article/${item.id}`}>{item.title}</Link>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Card>
+  );
 }
 
 export default BlogCard;
