464eee229286fbe4961b3b39b1de1d29aa83a8cc
tohid <tohidebrahimi17@gmail.com>
2022-08-19 17:57:03 +0430

    feat(shoppingCart):add breadCrumbs component

--- src/Pages/Main/Cart/Cart.tsx
+++ src/Pages/Main/Cart/Cart.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Breadcrumbs } from "../../../Components";
 import ShoppingCart from "../../../Components/ShoppingCart/ShoppingCart";
 
 const Cart = () => {
-    return (
-        <>
-            <ShoppingCart/>
-        </>
-    );
+  return (
+    <>
+      <Breadcrumbs title={"cart"} />
+      <ShoppingCart />
+    </>
+  );
 };
 
 export default Cart;
