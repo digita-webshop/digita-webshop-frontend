fd4046753742ac54d081e8a7d13496763ba6625c
monire <aglida1370@gmail.com>
2022-08-26 23:12:54 +0430

    feet(rtk): added basic request

--- src/Pages/Main/Cart/Cart.tsx
+++ src/Pages/Main/Cart/Cart.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Breadcrumbs } from "../../../Components";
 import ShoppingCart from "../../../Components/ShoppingCart/ShoppingCart";
-
+import {useGetPokemonByNameQuery} from '../../../api/cart'
 const Cart = () => {
+
+    const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
+
+    console.log('zz',data)
+
   return (
     <>
       <Breadcrumbs title={"cart"} />
