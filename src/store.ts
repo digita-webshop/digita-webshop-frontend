6ea2d1873abaae37bb64baad1db324cf3748cc96
tohid <tohidebrahimi17@gmail.com>
2022-08-27 17:27:08 +0430

    feat(productsApi):injected all products endpoints

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reducers from './features/_rootReducer';
+import reducers from "./features/_rootReducer";
+import { api } from "./features/api";
 
 const store = configureStore({
-    reducer: reducers,
-    devTools: process.env.NODE_ENV !== 'production',
-    preloadedState: {},
+  reducer: { ...reducers, [api.reducerPath]: api.reducer },
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+  preloadedState: {},
 });
 
 export default store;
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
