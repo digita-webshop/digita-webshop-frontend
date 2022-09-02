2a69b4b6e24c13d6e39fc4f1e694d3b716c79559
Siavash Khani <siavash.khani200@gmail.com>
2022-09-02 16:09:54 +0430

    fix(Protected): logic update

--- src/Components/Protected/Protected.tsx
+++ src/Components/Protected/Protected.tsx
@@ -1,19 +1,34 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 interface Props {
   role: null | string;
   children: JSX.Element;
 }
 
 const Protected = ({ role, children }: Props) => {
-  if (role === "admin") {
-    return <Navigate to="/panel/*" replace />;
-  } else if (role === "user") {
-    return <Navigate to="/user/*" replace />;
-  } /* else if (role === "superadmin") {
-    return <Navigate to="/panel/manage-access" replace />;
-  } */ else if (role === null) {
+  const location = useLocation().pathname.split("/");
+
+  //* Admin dont have access to user
+  if (role === "admin" && location[1] !== "user") {
+    return children;
+  }
+
+  //* User dont have access to panel
+  else if (role === "user" && location[1] !== "panel") {
+    return <Navigate to="/user" replace />;
+  }
+
+  //* User dont access to panel and admin dont have access to user routes
+  else if (
+    (role === "user" && location[1] === "panel") ||
+    (role === "admin" && location[1] === "user")
+  ) {
+    return <Navigate to="/" replace />;
+  } else if (role === null) {
     return <Navigate to="/" replace />;
   }
+  /* else if (role === "superadmin") {
+    return <Navigate to="/panel/manage-access" replace />;
+  } */
   return children;
 };
 
