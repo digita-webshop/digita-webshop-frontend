c616b2f4088123be88d7c89fd562046b7a6f91d3
tohid <tohidebrahimi17@gmail.com>
2023-01-06 11:43:26 +0330

    feat(home): add react helmet

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,12 @@
+import { Helmet } from "react-helmet-async";
 import { Products, Special, Header, ShopByCategories, WhatClientSay, OurBlog } from "./Components";
 
 function Home() {
   return (
     <>
+      <Helmet>
+        <title>Digita Online Shop </title>
+      </Helmet>
       <Header />
       <Products />
       <ShopByCategories />
