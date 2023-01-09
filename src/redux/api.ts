5c95dea4894980baafa4e06996d9e34f5050904b
tohid ebrahimi <tohidebrahimi17@gmail.com>
2023-01-09 12:03:14 +0330

    feat(api): update base url

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://digita-api.iran.liara.run/api" }),
   tagTypes: [
     "Product",
     "Article",
