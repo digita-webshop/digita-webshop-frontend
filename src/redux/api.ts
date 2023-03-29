d4ee384f58259d72b260d98a415161d6f6a52fa9
tohiidd <tohidebrahimi17@gmail.com>
2023-03-29 11:40:02 +0430

    fix(api):set additional headers

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -2,7 +2,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://digita-api.iran.liara.run/api",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  }),
+
   tagTypes: ["Product", "Article", "Wishlist", "Cart", "User", "Users", "Orders"],
   endpoints: () => ({}),
 });
